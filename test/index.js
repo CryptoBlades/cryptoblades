5360f152a3c7f4ea8aecaf43f66086de0b96a40c
Raymond Hammarling <raymond@hammarlings.se>
2021-03-13 13:36:37 +0100

    Add some tests to experiment with

--- test/index.js
+++ test/index.js
@@ -1,23 +1,51 @@
 const Kryptoknights = artifacts.require('Kryptoknights');
+const Characters = artifacts.require('Characters');
 
 contract('Kryptoknights', accounts => {
-  describe('_name', () => {
-    it('contains string', async () => {
+  describe('mintCharacter', () => {
+    it('should work', async () => {
       const instance = await Kryptoknights.deployed();
 
-      const res = await instance._name.call();
+      const res = await instance.mintCharacter({ from: accounts[2], value: web3.utils.toWei("5", "ether") });
 
-      assert.equal(res, 'Krypto knights!');
+      assert.equal(res.logs[0].event, 'NewCharacter');
+      assert.equal(res.logs[0].args.character.toNumber(), 0);
     });
   });
 
-  describe('getName', () => {
-    it('should return string', async () => {
+  describe('characters.getRequiredXpForLevel', () => {
+    it('should work', async () => {
       const instance = await Kryptoknights.deployed();
 
-      const res = await instance.getName.call();
+      const charactersContractAddress = await instance.characters.call();
 
-      assert.equal(res, 'Krypto knights!');
+      const characters = await Characters.at(charactersContractAddress);
+
+      const requiredXp = await characters.getRequiredXpForLevel.call(3);
+
+      assert.strictEqual(requiredXp.toNumber(), 18);
+    });
+  });
+
+  describe('characters.setXp', () => {
+    it('should work', async () => {
+      const instance = await Kryptoknights.deployed();
+
+      const mintRes = await instance.mintCharacter({ from: accounts[1], value: web3.utils.toWei("5", "ether") });
+
+      const characterId = mintRes.logs[0].args.character.toNumber();
+
+      const charactersContractAddress = await instance.characters.call();
+
+      const characters = await Characters.at(charactersContractAddress);
+
+      try {
+        await characters.setXp(characterId, 1000);
+        assert.fail('Expected setXp to throw error');
+      }
+      catch(e) {
+        assert.match(e.message, /Reason given: Can only be called by main file/g);
+      }
     });
   });
 });
