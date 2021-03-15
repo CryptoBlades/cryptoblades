f63831bd30431315ad8086a83b1402f9da9e2f0c
PoshDan <karsai.daniel.93@gmail.com>
2021-03-15 16:37:00 +0100

    skill token change

--- contracts/data.sol
+++ contracts/data.sol
@@ -6,17 +6,19 @@ contract Data is Util {
 
     uint256 public maxStamina = 200;
     uint256 public secondsPerStamina = 300; //5 * 60
-
-    uint256 public mintCharacterFee = 5 ether;
-    uint256 public rerollTraitFee = 5 ether;
-    uint256 public rerollCosmeticsFee = 5 ether;
-    uint256 public refillStaminaFee = 10 ether;
-
+    
+    uint256 public mintCharacterFee = 500;
+    uint256 public rerollTraitFee = 300;
+    uint256 public rerollCosmeticsFee = 300;
+    uint256 public refillStaminaFee = 1000;
+    
     uint256 public mintWeaponFee = 200;
     uint256 public reforgeWeaponFee = 100;
 
     function getStaminaMaxWait() public view returns (uint64) {
         return uint64(maxStamina * secondsPerStamina);
     }
+
+    
 
 }
--- contracts/kryptoknights.sol
+++ contracts/kryptoknights.sol
@@ -15,11 +15,46 @@ contract Kryptoknights is Data {
         weapons = new Weapons(address(this));
     }
 
+    mapping (address => uint256) public skill;
+    
     event FightOutcome(uint256 character, uint16 target, uint16 outcome);
     event LevelUp(uint256 character, uint16 level);
     event NewCharacter(uint256 character);
     event NewWeapon(uint256 weapon);
 
+    function getCharactersAddress() external view returns (address) {
+        return address(characters);
+    }
+    
+    function getWeaponsAddress() external view returns (address) {
+        return address(weapons);
+    }
+
+    function getSkill() external view returns (uint256) {
+        return skill[msg.sender];
+    }
+
+    function giveMeSkill(uint256 amount) public {
+        // TEMPORARY FUNCITON TO TEST WITH
+        skill[msg.sender] = skill[msg.sender] + amount;
+    }
+
+    function getCharacters() public view returns(uint256[] memory) {
+        uint256[] memory tokens = new uint256[](characters.balanceOf(msg.sender));
+        for(uint256 i = 0; i < tokens.length; i++) {
+            tokens[i] = characters.tokenOfOwnerByIndex(msg.sender, i);
+        }
+        return tokens;
+    }
+    
+    function getWeapons() public view returns(uint256[] memory) {
+        uint256[] memory tokens = new uint256[](weapons.balanceOf(msg.sender));
+        for(uint256 i = 0; i < tokens.length; i++) {
+            tokens[i] = weapons.tokenOfOwnerByIndex(msg.sender, i);
+        }
+        return tokens;
+    }
+    
     function fight(uint256 character, uint256 weapon, uint16 target) external isCharacterOwner(character) isWeaponOwner(weapon) isTargetValid(character, target)  {
         // todo
         uint atk;
@@ -34,22 +69,26 @@ contract Kryptoknights is Data {
         // 4 targets: 1 easy, 2 mediums/mediumhards, 1 hard?
         return [uint16(0),0,0,0];
     }
-
-    function mintCharacter() public payable requiredPay(mintCharacterFee) {
+    
+    function mintCharacter() public {
+        paySkill(mintCharacterFee);
         uint256 tokenID = characters.mint();
         emit NewCharacter(tokenID);
     }
-
-    function mintWeapon() public payable requiredPay(mintWeaponFee) {
+    
+    function mintWeapon() public {
+        paySkill(mintWeaponFee);
         uint256 tokenID = weapons.mint();
         emit NewWeapon(tokenID);
     }
-
-    function fillStamina(uint256 character) public payable isCharacterOwner(character) requiredPay(refillStaminaFee) {
+    
+    function fillStamina(uint256 character) public isCharacterOwner(character) {
+        paySkill(refillStaminaFee);
         characters.setStaminaTimestamp(character, characters.getStaminaTimestamp(character) - getStaminaMaxWait());
     }
-
-    function reforgeWeapon(uint256 reforgeID, uint256 burnID) public payable requiredPay(reforgeWeaponFee) {
+    
+    function reforgeWeapon(uint256 reforgeID, uint256 burnID) public {
+        paySkill(reforgeWeaponFee);
 
         uint8 burnWeaponStar = weapons.getStars(burnID);
         uint16 xpGain = (burnWeaponStar - 1) * 8 + 16;
@@ -79,10 +118,9 @@ contract Kryptoknights is Data {
         require(foundMatch, "Target invalid");
         _;
     }
-
-    modifier requiredPay(uint256 amount) {
-        require(msg.value == amount, "Incorrect payment");
-        _;
+    
+    function paySkill(uint256 amount) internal {
+        require(skill[msg.sender] >= amount, string(abi.encodePacked("Not enough SKILL! Need ",(amount))));
+        skill[msg.sender] = skill[msg.sender] - amount;
     }
-
 }
