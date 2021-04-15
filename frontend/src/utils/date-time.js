8e17d01ddfa2500c472d9320d9d5fdf18e4e593c
Raymond Hammarling <raymond@hammarlings.se>
2021-04-15 03:24:19 +0200

    Fix time duration formatting

--- frontend/src/utils/date-time.js
+++ frontend/src/utils/date-time.js
@@ -3,22 +3,21 @@ const SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE;
 const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
 
 function formatDurationToUnit(n, unitNames) {
-  if (n > 1) {
-    return n.toFixed(0) + ' ' + (n > 2 ? unitNames[1] : unitNames[0]);
-  }
-  return '';
+  return n.toFixed(0) + ' ' + (n.toFixed(0) === '1' ? unitNames[0] : unitNames[1]);
 }
 
+window.formatDurationFromSeconds = formatDurationFromSeconds;
+
 export function formatDurationFromSeconds(sec) {
-  if (sec / SECONDS_IN_DAY > 1) {
+  if (sec / SECONDS_IN_DAY >= 1) {
     return formatDurationToUnit(sec / SECONDS_IN_DAY, ['day', 'days']);
   }
 
-  if (sec / SECONDS_IN_HOUR > 1) {
+  if (sec / SECONDS_IN_HOUR >= 1) {
     return formatDurationToUnit(sec / SECONDS_IN_HOUR, ['hour', 'hours']);
   }
 
-  if (sec / SECONDS_IN_MINUTE > 1) {
+  if (sec / SECONDS_IN_MINUTE >= 1) {
     return formatDurationToUnit(sec / SECONDS_IN_MINUTE, ['minute', 'minutes']);
   }
 
