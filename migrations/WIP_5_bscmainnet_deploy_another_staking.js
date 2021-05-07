da642360055e419630f24cbdc62e6b8e7b71bfd0
Raymond Hammarling <raymond@hammarlings.se>
2021-05-07 02:21:07 +0200

    Checksummify addresses

--- migrations/WIP_5_bscmainnet_deploy_another_staking.js
+++ migrations/WIP_5_bscmainnet_deploy_another_staking.js
@@ -5,8 +5,8 @@ module.exports = async function (deployer, network) {
     const ownerAddress = '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48';
     const rewardDistributorAddress = '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48';
 
-    const skillTokenAddress = '0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab';
-    const lpTokenAddress = '0xc19dfd34d3ba5816df9cbdaa02d32a9f8dc6f6fc';
+    const skillTokenAddress = '0x154A9F9cbd3449AD22FDaE23044319D6eF2a1Fab';
+    const lpTokenAddress = '0xC19dfd34D3ba5816dF9CBDaa02D32A9F8dc6F6fC';
 
     await deployProxy(LP2StakingRewardsUpgradeable, [ownerAddress, rewardDistributorAddress, skillTokenAddress, lpTokenAddress, 0], { deployer });
   }
