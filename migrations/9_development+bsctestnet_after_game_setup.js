10d03ddcd5bbb0941a81edc228d046752c30c207
Raymond Hammarling <raymond@hammarlings.se>
2021-05-27 14:10:32 +0200

    Do dev setup on testnet too, give SKILL to specific accounts just in case

--- migrations/9_development+bsctestnet_after_game_setup.js
+++ migrations/9_development+bsctestnet_after_game_setup.js
@@ -3,12 +3,15 @@ const CryptoBlades = artifacts.require("CryptoBlades");
 const BasicPriceOracle = artifacts.require("BasicPriceOracle");
 
 module.exports = async function (deployer, network) {
-  if (network === 'development' || network === 'development-fork') {
+  if (network === 'development' || network === 'development-fork' || network === 'bsctestnet' || network === 'bsctestnet-fork') {
     const token = await SkillToken.deployed();
     const game = await CryptoBlades.deployed();
     await token.transferFrom(token.address, game.address, web3.utils.toWei('0.5', 'mether')); // megaether
 
     const priceOracle = await BasicPriceOracle.deployed();
     await priceOracle.setCurrentPrice(web3.utils.toWei('0.2', 'ether')); // 1/5 SKILL per USD, AKA 5 USD per SKILL
+
+    await token.transferFrom(token.address, '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48', web3.utils.toWei('100', 'kether')); // Philip Devine
+    await token.transferFrom(token.address, '0xa2bB660A6A3Bb5c74E36415ffe5D4862eFfc417A', web3.utils.toWei('100', 'kether')); // Raymond H.
   }
 };
