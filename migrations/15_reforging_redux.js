745aceda60ddd336739898b169a58ea4d9268f70
Raymond Hammarling <raymond@hammarlings.se>
2021-06-05 01:18:49 +0200

    Deploy to testnet

---
+++ migrations/15_reforging_redux.js
@@ -0,0 +1,18 @@
+const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+const CryptoBlades = artifacts.require("CryptoBlades");
+const Characters = artifacts.require("Characters");
+const Weapons = artifacts.require("Weapons");
+
+module.exports = async function (deployer) {
+  const charas = await Characters.deployed();
+  await upgradeProxy(charas.address, Characters, { deployer });
+
+  const weps = await Weapons.deployed();
+  const newWeps = await upgradeProxy(weps.address, Weapons, { deployer });
+
+  await newWeps.migrateTo_e55d8c5();
+
+  const game = await CryptoBlades.deployed();
+  await upgradeProxy(game.address, CryptoBlades, { deployer });
+};
