e0910b6cb1eafca17dc246356675c2fe596b2eb1
Raymond Hammarling <raymond@hammarlings.se>
2021-06-08 06:50:30 +0200

    Use IERC721 instead of address type

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -10,7 +10,6 @@ import "../node_modules/abdk-libraries-solidity/ABDKMath64x64.sol";
 import "./interfaces/IPriceOracle.sol";
 
 contract NFTMarket is Initializable, AccessControlUpgradeable {
-    
     using SafeMath for uint256;
     using ABDKMath64x64 for int128; // kroge beware
     using EnumerableSet for EnumerableSet.UintSet;
@@ -40,42 +39,42 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
     address public taxAddress;//game contract
     //IPriceOracle public priceOracleSkillPerUsd; // we may want this for dynamic pricing
 
-    address[] public listedTokenTypes; // stored for a way to know the types we have on offer
-    mapping(address => uint256) private tokenTypeIndex;
+    IERC721[] public listedTokenTypes; // stored for a way to know the types we have on offer
+    mapping(IERC721 => uint256) private tokenTypeIndex;
 
-    mapping(address => bool) public tokenBans;
+    mapping(IERC721 => bool) public tokenBans;
     mapping(address => bool) public userBans;
 
-    mapping(address => int128) public tax; // per NFT type tax
-    mapping(address => bool) private freeTax; // since tax is 0-default, this specifies it to fix an exploit
+    mapping(IERC721 => int128) public tax; // per NFT type tax
+    mapping(IERC721 => bool) private freeTax; // since tax is 0-default, this specifies it to fix an exploit
     int128 public defaultTax; // fallback in case we haven't specified it
 
-    mapping(address => uint256) private listingCounts; // helps with registering new types
-    mapping(address/*NFT address*/ => mapping(uint256/*ID*/ => Listing)) private listings;
-    mapping(address/*NFT address*/ => EnumerableSet.UintSet) private listedTokenIDs;
+    mapping(IERC721 => uint256) private listingCounts; // helps with registering new types
+    mapping(IERC721 => mapping(uint256/*ID*/ => Listing)) private listings;
+    mapping(IERC721 => EnumerableSet.UintSet) private listedTokenIDs;
 
-    event NewListing(address indexed seller, address indexed nftAddress, uint256 indexed nftID, uint256 price);
-    event ListingPriceChange(address indexed seller, address indexed nftAddress, uint256 indexed nftID, uint256 newPrice);
-    event CancelledListing(address indexed seller, address indexed nftAddress, uint256 indexed nftID);
-    event PurchasedListing(address indexed buyer, address seller, address indexed nftAddress, uint256 indexed nftID, uint256 price);
+    event NewListing(address indexed seller, IERC721 indexed nftAddress, uint256 indexed nftID, uint256 price);
+    event ListingPriceChange(address indexed seller, IERC721 indexed nftAddress, uint256 indexed nftID, uint256 newPrice);
+    event CancelledListing(address indexed seller, IERC721 indexed nftAddress, uint256 indexed nftID);
+    event PurchasedListing(address indexed buyer, address seller, IERC721 indexed nftAddress, uint256 indexed nftID, uint256 price);
 
     modifier restricted() {
         require(hasRole(GAME_ADMIN, msg.sender), "Not game admin");
         _;
     }
 
-    modifier isSeller(address tokenAddress, uint256 id) {
+    modifier isSeller(IERC721 tokenAddress, uint256 id) {
         require(listings[tokenAddress][id].seller == msg.sender, "Access denied");
         _;
     }
 
-    modifier isSellerOrAdmin(address tokenAddress, uint256 id) {
+    modifier isSellerOrAdmin(IERC721 tokenAddress, uint256 id) {
         require(listings[tokenAddress][id].seller == msg.sender
             || hasRole(GAME_ADMIN, msg.sender), "Access denied");
         _;
     }
 
-    modifier tokenNotBanned(address tokenAddress) {
+    modifier tokenNotBanned(IERC721 tokenAddress) {
         require(tokenBans[tokenAddress] == false, "This type of NFT may not be traded here");
         _;
     }
@@ -89,23 +88,21 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         skillToken.transfer(msg.sender, amount); // dont expect we'll hold tokens here but might as well
     }
 
-    function getListingIDs(address tokenAddress) public view returns (uint256[] memory) {
-        
+    function getListingIDs(IERC721 tokenAddress) public view returns (uint256[] memory) {
         EnumerableSet.UintSet storage set = listedTokenIDs[tokenAddress];
         uint256[] memory tokens = new uint256[](set.length());
-        
+
         for(uint256 i = 0; i < tokens.length; i++) {
             tokens[i] = set.at(i);
         }
         return tokens;
     }
 
-    function getNumberOfListingsBySeller(address tokenAddress, address seller) public view returns (uint256) {
+    function getNumberOfListingsBySeller(IERC721 tokenAddress, address seller) public view returns (uint256) {
         return getListingIDsBySeller(tokenAddress, seller).length;
     }
 
-    function getListingIDsBySeller(address tokenAddress, address seller) public view returns (uint256[] memory) {
-        
+    function getListingIDsBySeller(IERC721 tokenAddress, address seller) public view returns (uint256[] memory) {
         uint256[] memory listedTokens = getListingIDs(tokenAddress);
 
         // correct me if I'm wrong but we cant have dynamic arrays in memory?
@@ -117,7 +114,7 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         }
 
         uint256[] memory tokens = new uint256[](amount);
-        
+
         uint256 index = 0;
         for(uint256 i = 0; i < tokens.length; i++) {
             uint256 id = listedTokens[i];
@@ -126,35 +123,34 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         }
     }
 
-    function getNumberOfListingsByAdress(address tokenAddress) public view returns (uint256) {
+    function getNumberOfListingsForToken(IERC721 tokenAddress) public view returns (uint256) {
         return listingCounts[tokenAddress];
     }
 
-    function getSellerPrice(address tokenAddress, uint256 id) public view returns (uint256) {
+    function getSellerPrice(IERC721 tokenAddress, uint256 id) public view returns (uint256) {
         return listings[tokenAddress][id].price;
     }
 
-    function getFinalPrice(address tokenAddress, uint256 id) public view returns (uint256) {
+    function getFinalPrice(IERC721 tokenAddress, uint256 id) public view returns (uint256) {
         return getSellerPrice(tokenAddress, id).add(getTaxOnListing(tokenAddress, id));
     }
 
-    function getTaxOnListing(address tokenAddress, uint256 id) public view returns (uint256) {
+    function getTaxOnListing(IERC721 tokenAddress, uint256 id) public view returns (uint256) {
         return ABDKMath64x64.mulu(tax[tokenAddress], getSellerPrice(tokenAddress, id));
     }
 
-    function setTaxOnTokenType(address tokenAddress, int128 newTax) public restricted {
+    function setTaxOnTokenType(IERC721 tokenAddress, int128 newTax) public restricted {
         require(newTax >= 0, "We're not running a charity here");
         tax[tokenAddress] = newTax;
         freeTax[tokenAddress] = newTax == 0;
     }
 
-    function addListing(address tokenAddress, uint256 id, uint256 price) public
+    function addListing(IERC721 tokenAddress, uint256 id, uint256 price) public
             userNotBanned
             tokenNotBanned(tokenAddress) {
 
         // in theory the transfer and required approval already test non-owner operations
-        IERC721 tokenType = IERC721(tokenAddress);
-        tokenType.safeTransferFrom(msg.sender, address(this), id);
+        tokenAddress.safeTransferFrom(msg.sender, address(this), id);
         listings[tokenAddress][id] = Listing(msg.sender, price);
         listedTokenIDs[tokenAddress].add(id);
 
@@ -162,11 +158,11 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         if(listingCounts[tokenAddress] == 0)
             registerTokenAddress(tokenAddress);
         listingCounts[tokenAddress] = listingCounts[tokenAddress].add(1);
-        
+
         emit NewListing(msg.sender, tokenAddress, id, price);
     }
 
-    function changeListingPrice(address tokenAddress, uint256 id, uint256 newPrice) public
+    function changeListingPrice(IERC721 tokenAddress, uint256 id, uint256 newPrice) public
             userNotBanned
             isSeller(tokenAddress, id) {
 
@@ -174,11 +170,10 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         emit ListingPriceChange(msg.sender, tokenAddress, id, newPrice);
     }
 
-    function cancelListing(address tokenAddress, uint256 id) public
+    function cancelListing(IERC721 tokenAddress, uint256 id) public
             userNotBanned
             isSellerOrAdmin(tokenAddress, id) {
-        IERC721 tokenType = IERC721(tokenAddress);
-        tokenType.safeTransferFrom(address(this), msg.sender, id);
+        tokenAddress.safeTransferFrom(address(this), msg.sender, id);
         delete listings[tokenAddress][id];
         listedTokenIDs[tokenAddress].remove(id);
 
@@ -186,11 +181,11 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         if(listingCounts[tokenAddress] == 1)
             unregisterTokenAddress(tokenAddress);
         listingCounts[tokenAddress] = listingCounts[tokenAddress].sub(1);
-        
+
         emit CancelledListing(msg.sender, tokenAddress, id);
     }
-    
-    function purchaseListing(address tokenAddress, uint256 id, uint256 maxPrice) public
+
+    function purchaseListing(IERC721 tokenAddress, uint256 id, uint256 maxPrice) public
             userNotBanned
             tokenNotBanned(tokenAddress) {
         uint256 finalPrice = getFinalPrice(tokenAddress, id);
@@ -214,7 +209,7 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         emit PurchasedListing(msg.sender, listing.seller, tokenAddress, id, finalPrice);
     }
 
-    function registerTokenAddress(address tokenAddress) private {
+    function registerTokenAddress(IERC721 tokenAddress) private {
         tokenTypeIndex[tokenAddress] = listedTokenTypes.length;
         listedTokenTypes.push(tokenAddress);
 
@@ -224,9 +219,9 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
             tax[tokenAddress] = defaultTax;
     }
 
-    function unregisterTokenAddress(address tokenAddress) private {
+    function unregisterTokenAddress(IERC721 tokenAddress) private {
         uint256 lastIndex = listedTokenTypes.length.sub(1);
-        address movedTokenType = listedTokenTypes[lastIndex];
+        IERC721 movedTokenType = listedTokenTypes[lastIndex];
         uint256 movingIndex = tokenTypeIndex[tokenAddress];
 
         listedTokenTypes[movingIndex] = movedTokenType;
@@ -248,7 +243,7 @@ contract NFTMarket is Initializable, AccessControlUpgradeable {
         userBans[user] = to;
     }
 
-    function setTokenBan(address token, bool to) public restricted {
+    function setTokenBan(IERC721 token, bool to) public restricted {
         tokenBans[token] = to;
     }
 }
