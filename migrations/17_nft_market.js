c133087d57517f5b44711599a5695fea5571a528
Raymond Hammarling <raymond@hammarlings.se>
2021-06-08 11:42:55 +0200

    Deploy to mainnet

--- migrations/17_nft_market.js
+++ migrations/17_nft_market.js
@@ -7,7 +7,7 @@ const NFTMarket = artifacts.require("NFTMarket");
 module.exports = async function (deployer, network) {
   let skillTokenAddress;
   if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
-    skillTokenAddress = await IERC20.at('0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab');
+    skillTokenAddress = '0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab';
   }
   else {
     skillTokenAddress = SkillToken.address;
