e73b01046f2bd04806342227c6df03008baf2588
Raymond Hammarling <raymond@hammarlings.se>
2021-06-08 10:05:31 +0200

    Deploy to testnet

---
+++ migrations/17_nft_market.js
@@ -0,0 +1,17 @@
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+
+const SkillToken = artifacts.require("SkillToken");
+const CryptoBlades = artifacts.require("CryptoBlades");
+const NFTMarket = artifacts.require("NFTMarket");
+
+module.exports = async function (deployer, network) {
+  let skillTokenAddress;
+  if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
+    skillTokenAddress = await IERC20.at('0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab');
+  }
+  else {
+    skillTokenAddress = SkillToken.address;
+  }
+
+  await deployProxy(NFTMarket, [skillTokenAddress, CryptoBlades.address], { deployer });
+};
