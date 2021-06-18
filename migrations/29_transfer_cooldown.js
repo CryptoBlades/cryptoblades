970e714dc264c63d28e492f7fdc9c26ae17802fa
Raymond Hammarling <raymond@hammarlings.se>
2021-06-19 00:36:55 +0200

    Update WIP migration

--- migrations/29_transfer_cooldown.js
+++ migrations/29_transfer_cooldown.js
@@ -2,6 +2,7 @@ const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 const Characters = artifacts.require("Characters");
 const Weapons = artifacts.require("Weapons");
+const NFTMarket = artifacts.require("NFTMarket");
 
 module.exports = async function (deployer) {
   const charas = await Characters.deployed();
@@ -11,4 +12,11 @@ module.exports = async function (deployer) {
   const weps = await Weapons.deployed();
   const newWeps = await upgradeProxy(weps.address, Weapons, { deployer });
   await newWeps.migrateTo_951a020();
+
+  const RECEIVE_DOES_NOT_SET_TRANSFER_TIMESTAMP = await newWeps.RECEIVE_DOES_NOT_SET_TRANSFER_TIMESTAMP();
+
+  await Promise.all([
+    newWeps.grantRole(RECEIVE_DOES_NOT_SET_TRANSFER_TIMESTAMP, NFTMarket.address),
+    newCharas.grantRole(RECEIVE_DOES_NOT_SET_TRANSFER_TIMESTAMP, NFTMarket.address),
+  ]);
 };
