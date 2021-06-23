eed1b9e6ab2c6de831a4cbb0f807226d049f6c69
Arthon van Dijk <arthon@bouw7.nl>
2021-06-24 01:12:09 +0200

    [TASK] Fix indentations.

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -253,12 +253,12 @@ export function createStore(web3: Web3) {
         }
 
         return(`${
-                (date.getMonth()+1).toString().padStart(2, '0')}/${
-                date.getDate().toString().padStart(2, '0')}/${
-                date.getFullYear().toString().padStart(4, '0')} ${
-                date.getHours().toString().padStart(2, '0')}:${
-                date.getMinutes().toString().padStart(2, '0')}:${
-                date.getSeconds().toString().padStart(2, '0')}`
+          (date.getMonth()+1).toString().padStart(2, '0')}/${
+          date.getDate().toString().padStart(2, '0')}/${
+          date.getFullYear().toString().padStart(4, '0')} ${
+          date.getHours().toString().padStart(2, '0')}:${
+          date.getMinutes().toString().padStart(2, '0')}:${
+          date.getSeconds().toString().padStart(2, '0')}`
         );
       },
 
