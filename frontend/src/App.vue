969c5831cd52e4baa3e5cd4cd13a408dac4025b9
newb23 <newb23@gmail.com>
2021-06-24 23:17:01 -0600

    MetaMask Integration
    Added the ability to change the network with a  button-press.
    
    MetaMask Integration
    
    Adds MetaMask Integration to the Initial Setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,65 +1,62 @@
 <template>
   <div class="app">
     <nav-bar />
-
     <claim-rewards-bar v-if="canShowRewardsBar" />
-
     <character-bar v-if="!featureFlagStakeOnly && currentCharacterId !== null" />
-
     {{ typeof skillBalance }}
-
     <div class="content dark-bg-text">
       <router-view v-if="canShowApp" />
     </div>
-
     <div class="fullscreen-warning" v-if="showMetamaskWarning">
       <div class="starter-panel">
         <span class="starter-panel-heading">Metamask Not Detected</span>
-
-        <div class="seperator"></div>
-
-        <div class="instructions-list">
-          <p>
-            You need Metamask installed to use this app. <a target="_blank" href="https://stakingbits.com/setting-up-metamask-for-binance-smart-chain-bsc-921d9a2625fd">You can find instructions on how to set it up here.</a>
-          </p>
+        <div class="center">
+          <big-button class="button" :mainText="`Add MetaMask`" @click="startOnbording" />
         </div>
       </div>
     </div>
-
     <div class="fullscreen-warning" v-if="!showMetamaskWarning && (errorMessage || ownCharacters.length === 0)">
       <div class="starter-panel">
         <img class="mini-icon-starter" src="./assets/placeholder/sword-placeholder-6.png" alt="" srcset="" />
         <span class="starter-panel-heading">{{ errorMessage || 'Get Started With CryptoBlades' }}</span>
         <img class="mini-icon-starter" src="./assets/placeholder/sword-placeholder-6.png" alt="" srcset="" />
-
+        <big-button class="button" :mainText="`Add SKILL Tokens to MetaMask`" @click="configureMetaMask" />
         <div class="seperator"></div>
-
         <div class="instructions-list">
           <p>
-            Get started in less than 10 minutes!
-            To recruit your first character you need 5 Skill and .001 BNB for gas.
-            You will also need .0015 BNB to do your first few battles, but don't worry,
-            you earn the battle fees back in SKILL rewards immediately!
+            Get started in less than 10 minutes! To recruit your first character you need 5 Skill and .001 BNB for gas. You will also need .0015 BNB to do your
+            first few battles, but don't worry, you earn the battle fees back in SKILL rewards immediately!
           </p>
           <ul class="unstyled-list">
-            <li>1. Buying BNB with fiat: <a href="https://youtu.be/6-sUDUE2RPA" target="_blank" rel="noopener noreferrer">Watch Video</a> </li>
-            <li>2. Once you have BNB, go to ApeSwap to obtain SKILL tokens:<br/> <a href="https://app.apeswap.finance/#/swap?outputCurrency=0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab">Trade SKILL/BNB</a></li>
+            <li>1. Buying BNB with fiat: <a href="https://youtu.be/6-sUDUE2RPA" target="_blank" rel="noopener noreferrer">Watch Video</a></li>
+            <li>
+              2. Once you have BNB, go to ApeSwap to obtain SKILL tokens:<br />
+              <a href="https://app.apeswap.finance/#/swap?outputCurrency=0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab">Trade SKILL/BNB</a>
+            </li>
             <li>3. Read the alert and select “I understand” and “Continue”</li>
-            <li>4. Follow this tutorial to swap BNB for SKILL: <a href="https://youtu.be/_zitrvJ7Hl4" target="_blank" rel="noopener noreferrer">Watch Video</a></li>
-            <li>5. That's it! Now you can create your first character: (<a href="https://youtu.be/ZcNq0jCa28c" target="_blank" rel="noopener noreferrer">Watch 'Getting Started' Video</a>)</li>
+            <li>
+              4. Follow this tutorial to swap BNB for SKILL: <a href="https://youtu.be/_zitrvJ7Hl4" target="_blank" rel="noopener noreferrer">Watch Video</a>
+            </li>
+            <li>
+              5. That's it! Now you can create your first character: (<a href="https://youtu.be/ZcNq0jCa28c" target="_blank" rel="noopener noreferrer"
+                >Watch 'Getting Started' Video</a
+              >)
+            </li>
           </ul>
-
-          <p>If you have any questions, please join our Discord: <a href="https://discord.gg/c5afzyQ3Q9" target="_blank" rel="noopener noreferrer">https://discord.gg/c5afzyQ3Q9</a></p>
+          <p>
+            If you have any questions, please join our Discord:
+            <a href="https://discord.gg/c5afzyQ3Q9" target="_blank" rel="noopener noreferrer">https://discord.gg/c5afzyQ3Q9</a>
+          </p>
         </div>
       </div>
     </div>
-
     <div class="fullscreen-warning" v-if="showNetworkError">
       <div>
         You are currently on the incorrect network.
       </div>
       <div>
-        Please switch to <span class="bold">{{ expectedNetworkName }}</span>.
+        Please switch to <span class="bold">{{ expectedNetworkName }}</span
+        >.
       </div>
     </div>
   </div>
@@ -68,9 +65,9 @@
 <script>
 import { mapState, mapActions, mapGetters } from 'vuex';
 import _ from 'lodash';
-
 import Events from './events';
-
+import MetaMaskOnboarding from '@metamask/onboarding';
+import BigButton from './components/BigButton.vue';
 import NavBar from './components/NavBar.vue';
 import CharacterBar from './components/CharacterBar.vue';
 import ClaimRewardsBar from './components/smart/ClaimRewardsBar.vue';
@@ -80,12 +77,13 @@ export default {
   components: {
     NavBar,
     CharacterBar,
-    ClaimRewardsBar
+    ClaimRewardsBar,
+    BigButton,
   },
 
   data: () => ({
     errorMessage: '',
-    canShowRewardsBar: true
+    canShowRewardsBar: true,
   }),
 
   computed: {
@@ -102,7 +100,7 @@ export default {
 
     showNetworkError() {
       return this.expectedNetworkId && this.currentNetworkId !== null && this.currentNetworkId !== this.expectedNetworkId;
-    }
+    },
   },
 
   watch: {
@@ -121,11 +119,11 @@ export default {
       'fetchCharacterStamina',
       'pollAccountsAndNetwork',
       'fetchWeaponTransferCooldownForOwnWeapons',
-      'fetchCharacterTransferCooldownForOwnCharacters'
+      'fetchCharacterTransferCooldownForOwnCharacters',
     ]),
 
     async updateCurrentCharacterStamina() {
-      if(this.featureFlagStakeOnly) return;
+      if (this.featureFlagStakeOnly) return;
 
       if (this.currentCharacterId !== null) {
         await this.fetchCharacterStamina(this.currentCharacterId);
@@ -134,7 +132,65 @@ export default {
 
     checkStorage() {
       this.canShowRewardsBar = !localStorage.getItem('rewards');
-    }
+    },
+    async startOnbording() {
+      const onboarding = new MetaMaskOnboarding();
+      onboarding.startOnboarding();
+    },
+    async configureMetaMask() {
+      const web3 = this.web3.currentProvider;
+
+      try {
+        await web3.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x38' }],
+        });
+      } catch (switchError) {
+        try {
+          await web3.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x38',
+                chainName: 'Binance Smart Chain Mainnet',
+                nativeCurrency: {
+                  name: 'Binance Coin',
+                  symbol: 'BNB',
+                  decimals: 18,
+                },
+                rpcUrls: ['https://bsc-dataseed.binance.org/'],
+                blockExplorerUrls: ['https://bscscan.com/'],
+              },
+            ],
+          });
+        } catch (addError) {
+          console.log(addError);
+        }
+      }
+
+      try {
+        const wasAdded = await web3.request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20',
+            options: {
+              address: '0x154a9f9cbd3449ad22fdae23044319d6ef2a1fab',
+              symbol: 'SKILL',
+              decimals: 18,
+              image: 'http://placekitten.com/200/300',
+            },
+          },
+        });
+
+        if (wasAdded) {
+          console.log('Prepare thyself!');
+        } else {
+          console.log('Token not added!');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 
   mounted() {
@@ -146,9 +202,9 @@ export default {
   async created() {
     try {
       await this.initializeStore();
-    } catch(e) {
+    } catch (e) {
       this.errorMessage = 'Welcome to CryptoBlades. Here\'s how you can get started.';
-      if(e.code === 4001) {
+      if (e.code === 4001) {
         this.errorMessage = 'Error: MetaMask could not get permissions.';
       }
 
@@ -161,15 +217,12 @@ export default {
     }, 3000);
 
     this.weaponTransferCooldownPollIntervalId = setInterval(async () => {
-      await Promise.all([
-        this.fetchCharacterTransferCooldownForOwnCharacters(),
-        this.fetchWeaponTransferCooldownForOwnWeapons()
-      ]);
+      await Promise.all([this.fetchCharacterTransferCooldownForOwnCharacters(), this.fetchWeaponTransferCooldownForOwnWeapons()]);
     }, 10 * 1000);
 
     this.doPollAccounts = true;
     const pollAccounts = async () => {
-      if(!this.doPollAccounts) return;
+      if (!this.doPollAccounts) return;
 
       try {
         await this.pollAccountsAndNetwork();
@@ -193,7 +246,7 @@ export default {
 <style>
 body {
   margin: 0;
-  background: linear-gradient(45deg, rgba(20,20,20,1) 0%, rgba(36,39,32,1) 100%);
+  background: linear-gradient(45deg, rgba(20, 20, 20, 1) 0%, rgba(36, 39, 32, 1) 100%);
 }
 
 .no-margin {
@@ -205,7 +258,7 @@ body {
 }
 
 .main-font {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 .title-bg-text {
@@ -241,46 +294,54 @@ button {
   color: red;
 }
 
-.fire, .str {
+.fire,
+.str {
   color: red;
 }
 
-.earth, .dex {
+.earth,
+.dex {
   color: green;
 }
 
-.water, .int {
+.water,
+.int {
   color: cyan;
 }
 
-.lightning, .cha {
+.lightning,
+.cha {
   color: yellow;
 }
 
-.fire-icon, .str-icon {
+.fire-icon,
+.str-icon {
   color: red;
-  content: url("assets/elements/fire.png");
+  content: url('assets/elements/fire.png');
   width: 1em;
   height: 1em;
 }
 
-.earth-icon, .dex-icon {
+.earth-icon,
+.dex-icon {
   color: green;
-  content: url("assets/elements/earth.png");
+  content: url('assets/elements/earth.png');
   width: 1em;
   height: 1em;
 }
 
-.water-icon, .int-icon {
+.water-icon,
+.int-icon {
   color: cyan;
-  content: url("assets/elements/water.png");
+  content: url('assets/elements/water.png');
   width: 1em;
   height: 1em;
 }
 
-.lightning-icon, .cha-icon {
+.lightning-icon,
+.cha-icon {
   color: yellow;
-  content: url("assets/elements/lightning.png");
+  content: url('assets/elements/lightning.png');
   width: 1em;
   height: 1em;
 }
@@ -351,7 +412,8 @@ button.close {
   background: linear-gradient(180deg, rgba(31, 31, 34, 1) 0%, rgba(24, 27, 30, 1) 5%, rgba(24, 38, 45, 1) 100%);
 }
 
-.nav-tabs .nav-link:hover, .nav-tabs .nav-link:focus {
+.nav-tabs .nav-link:hover,
+.nav-tabs .nav-link:focus {
   border-color: #9e8a57 #9e8a57 #9e8a57 !important;
 }
 
@@ -363,7 +425,6 @@ button.close {
 div.bg-success {
   background-color: #19682b !important;
 }
-
 </style>
 <style scoped>
 .app {
@@ -373,8 +434,8 @@ div.bg-success {
 .content {
   padding: 0 1em;
   height: calc(100vh - 56px);
-  background: rgb(20,20,20);
-  background: linear-gradient(45deg, rgba(20,20,20,1) 0%, rgba(36,39,32,1) 100%);
+  background: rgb(20, 20, 20);
+  background: linear-gradient(45deg, rgba(20, 20, 20, 1) 0%, rgba(36, 39, 32, 1) 100%);
 }
 
 .fullscreen-warning {
@@ -397,7 +458,7 @@ div.bg-success {
   max-width: 28em;
   background: rgba(0, 0, 0, 1);
   box-shadow: 0 2px 4px #ffffff38;
-  border :1px solid #9e8a57;
+  border: 1px solid #9e8a57;
   border-radius: 5px;
   padding: 0.5em;
   margin: auto;
@@ -406,8 +467,8 @@ div.bg-success {
 }
 
 .starter-panel-heading {
-  margin-left : 15px;
-  font-size : 45px;
+  margin-left: 15px;
+  font-size: 45px;
 }
 
 .starter-msg {
@@ -415,8 +476,8 @@ div.bg-success {
 }
 .instructions-list {
   text-align: start;
-  padding : 15px;
-  font-size : 0.5em;
+  padding: 15px;
+  font-size: 0.5em;
 }
 
 .unstyled-list {
@@ -425,12 +486,18 @@ div.bg-success {
 .seperator {
   border: 1px solid #9e8a57;
   border-radius: 3px;
-  width : 100%;
+  width: 100%;
 }
 
 .mini-icon-starter {
   height: 1.2em;
   width: 1.2em;
-  margin : 5px;
+  margin: 5px;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
