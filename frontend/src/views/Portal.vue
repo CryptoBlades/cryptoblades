809085b8aefa2db7b1fb48b08ead373d92036339
Kyle J. Kemp <kyle@seiyria.com>
2021-07-12 16:36:38 -0500

    Character hot swapper improvements (#224) (#244)
    * WAX portal improvements 1
    
    Adds additional regex blocking, error messaging, and WAX balance view.
    
    Co-authored-by: newb23 <newb23@gmail.com>

--- frontend/src/views/Portal.vue
+++ frontend/src/views/Portal.vue
@@ -16,11 +16,11 @@
         <big-button class="button" v-if="userAccount === null" mainText="WAX Login" @click="waxLogin" />
       </div>
       <div class="blank-slate" v-if="userAccount != null">
-        How Much Wax?
+        How much WAX do you want to transfer? You have: {{ waxBalance }}.
         <input v-model="WAXAmount" placeholder="How much WAX?" @input="change($event)" @change="change($event)" />
-        <div class="error" v-if="!isValid">Number is Invalid</div>
+        <div class="error" v-if="!isValid">Invalid WAX amount. Please use the format: "X.xxxxxxxx" (8 decimals).</div>
         <br />
-        <big-button class="button" v-if="userAccount != null" mainText="Submit" @click="sign" />
+        <big-button class="button" v-if="userAccount != null" :disabled="!isValid" mainText="Submit" @click="sign" />
       </div>
     </div>
   </div>
@@ -31,15 +31,19 @@ import BigButton from '../components/BigButton.vue';
 import * as waxjs from '@waxio/waxjs/dist';
 import { mapState } from 'vuex';
 const wax = new waxjs.WaxJS('https://wax.greymass.com', null, null, false);
+import BN from 'bignumber.js';
+
 export default {
   data() {
     return {
       userAccount: null,
       publicKey: null,
       WAXAmount: null,
-      regex: /^\d+(\.\d{8})?$/,
+      regex: /^\d+\.\d{8}$/,
       transactionResults: null,
       waitingResults: false,
+      waxBalance: 0,
+      isValid: null,
     };
   },
   computed: {
@@ -51,7 +55,9 @@ export default {
     async waxLogin() {
       this.userAccount = await wax.login();
       this.publicKey = wax.pubKeys;
+      this.getWaxBalance();
     },
+
     async sign() {
       if (!wax.api) {
         return document.getElementById('response').append('* Login first *');
@@ -73,7 +79,7 @@ export default {
                 data: {
                   from: wax.userAccount, //user's BSC Address
                   to: process.env.VUE_APP_WAX_BRIDGE_WAX_WALLET_ADDRESS, //CB Wallet Address
-                  quantity: this.WAXAmount + ' WAX', //WAX *needs* to be here.
+                  quantity: new BN(this.WAXAmount).toFixed(8) + ' WAX', //WAX *needs* to be here.
                   memo: this.defaultAccount,
                 },
               },
@@ -82,7 +88,7 @@ export default {
           {
             blocksBehind: 3,
             expireSeconds: 30,
-          }
+          },
         );
         this.transactionResults = results;
         this.waitingResults = false;
@@ -92,12 +98,18 @@ export default {
         this.error = e.message;
       }
     },
+
     async change(e) {
       const number = e.target.value;
       this.isNumberValid(number);
     },
+
     async isNumberValid(inputNumber) {
       this.isValid = this.regex.test(inputNumber);
+    },
+
+    async getWaxBalance() {
+      this.waxBalance = await wax.api.rpc.get_currency_balance('eosio.token', this.userAccount, 'WAX');
     },
   },
   components: {
