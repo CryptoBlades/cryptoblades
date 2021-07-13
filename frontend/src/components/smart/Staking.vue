4bc31ca0fcc587e909bd87f8b6925de5e5211926
Kacper <kacpertokarczyk1@gmail.com>
2021-07-13 11:39:31 +0200

    Staking unclaimed UX is

--- frontend/src/components/smart/Staking.vue
+++ frontend/src/components/smart/Staking.vue
@@ -218,7 +218,7 @@ export default {
     unlockTimeLeftInternal() { return this.stakeData.unlockTimeLeft; },
 
     stakingTokenName() {
-      return this.stakeType === 'skill' ? 'SKILL' : 'SKILL-WBNB';
+      return this.stakeType === 'skill' || this.stakeType === 'skill2' ? 'SKILL' : 'SKILL-WBNB';
     },
 
     minimumStakeTimeFormatted() {
