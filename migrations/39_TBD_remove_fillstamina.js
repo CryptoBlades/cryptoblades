46cde327118bdca70c0560bf7a5b5739de1e999c
Raymond Hammarling <raymond@hammarlings.se>
2021-07-16 21:05:25 +0200

    Remove migrations that I don't know how they stuck around

--- migrations/39_TBD_remove_fillstamina.js
+++
@@ -1,7 +0,0 @@
-const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-
-const CryptoBlades = artifacts.require("CryptoBlades");
-
-module.exports = async function (deployer, network, accounts) {
-  await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
-};
