8a918614b63f9c7e11511bb204ec4265634678ee
viruspowaa <virus.powaa@gmail.com>
2021-07-22 15:58:48 +0200

    feat(market): show sold banners over items that are sold but not removed from the DB yet
    close #332
    
    Co-authored-by: newb23 <newb23@gmail.com>

--- frontend/src/views/Market.vue
+++ frontend/src/views/Market.vue
@@ -52,17 +52,24 @@
 
                 <template #above="{ weapon: { id } }">
                   <div class="d-flex flex-column align-items-center justify-content-center m-top-negative-5">
-                    <span class="d-block text-center" v-if="nftPricesById[id]">
-                      <strong>Price</strong>: {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                    <span class="d-block text-center fix-h24" v-if="nftPricesById[id]">
+                      <span v-if="convertWeiToSkill(nftPricesById[id]) !== '0'">
+                        <strong>Price</strong>: {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                      </span>
                     </span>
                     <span class="d-block text-center" v-else>Loading price...</span>
                     <b-button
+                      :disabled="convertWeiToSkill(nftPricesById[id]) === '0'"
                       @click="selectedNftId = id; purchaseNft();"
                       variant="primary"
                       class="gtag-link-others">
-                      Purchase
+                      {{ convertWeiToSkill(nftPricesById[id]) !== '0' ? 'Purchase' : 'Sold' }}
                     </b-button>
                   </div>
+                </template>
+
+                <template #sold="{ weapon: { id } }">
+                  <div class="sold" v-if="nftPricesById[id] && convertWeiToSkill(nftPricesById[id]) === '0'"><span>sold</span></div>
                 </template>
 
               </weapon-grid>
@@ -79,21 +86,28 @@
 
                 <template #above="{ character: { id } }">
                   <div class="token-price d-flex flex-column align-items-center justify-content-center m-top-negative-50">
-                    <span class="d-block text-center" v-if="nftPricesById[id]">
-                       {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
-                     </span>
+                    <span class="d-block text-center fix-h24" v-if="nftPricesById[id]">
+                      <span v-if="convertWeiToSkill(nftPricesById[id]) !== '0'">
+                        {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                      </span>
+                    </span>
+
                     <span class="d-block text-center" v-else>Loading price...</span>
                     <b-button
+                      :disabled="convertWeiToSkill(nftPricesById[id]) === '0'"
                       @click="selectedNftId = id; canPurchase && purchaseNft();"
                       variant="primary"
                       v-bind:class="[!canPurchase ? 'disabled-button' : '']"
                       class="gtag-link-others" tagname="confirm_purchase">
-                      Purchase <b-icon-question-circle v-if="!canPurchase"
+                      {{ convertWeiToSkill(nftPricesById[id]) !== '0' ? 'Purchase' : 'Sold' }} <b-icon-question-circle v-if="!canPurchase"
                       v-tooltip.bottom="'You already have max amount of characters (4).'"/>
                     </b-button>
                   </div>
                 </template>
 
+                <template #sold="{ character: { id } }">
+                  <div class="sold" v-if="nftPricesById[id] && convertWeiToSkill(nftPricesById[id]) === '0'"><span>sold</span></div>
+                </template>
               </character-list>
 
               <b-pagination class="customPagination"
@@ -208,18 +222,25 @@
 
                 <template #above="{ weapon: { id } }">
                   <div class="d-flex flex-column align-items-center justify-content-center m-top-negative-5">
-                    <span class="d-block text-center" v-if="nftPricesById[id]">
-                      <strong>Price</strong>: {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                    <span class="d-block text-center fix-h24" v-if="nftPricesById[id]">
+                      <span v-if="convertWeiToSkill(nftPricesById[id]) !== '0'">
+                        <strong>Price</strong>: {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                      </span>
                     </span>
                     <span class="d-block text-center" v-else>Loading price...</span>
                     <b-button
                         v-if="id !== null && !searchResultsOwned"
+                        :disabled="convertWeiToSkill(nftPricesById[id]) === '0'"
                         @click="selectedNftId = id; purchaseNft();"
                         variant="primary"
                         class="gtag-link-others">
-                        Purchase
+                        {{ convertWeiToSkill(nftPricesById[id]) !== '0' ? 'Purchase' : 'Sold' }}
                     </b-button>
                   </div>
+                </template>
+
+                <template #sold="{ weapon: { id } }">
+                  <div class="sold" v-if="nftPricesById[id] && convertWeiToSkill(nftPricesById[id]) === '0'"><span>sold</span></div>
                 </template>
 
               </weapon-grid>
@@ -234,20 +255,27 @@
 
                 <template #above="{ character: { id } }">
                   <div class="token-price d-flex flex-column align-items-center justify-content-center m-top-negative-50">
-                    <span class="d-block text-center" v-if="nftPricesById[id]">
-                      {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                    <span class="d-block text-center fix-h24" v-if="nftPricesById[id]">
+                      <span v-if="convertWeiToSkill(nftPricesById[id]) !== '0'">
+                        {{ convertWeiToSkill(nftPricesById[id]) | maxDecimals(2) }} SKILL
+                      </span>
                     </span>
                     <span class="d-block text-center" v-else>Loading price...</span>
                     <b-button
                       v-if="id !== null && !searchResultsOwned"
+                      :disabled="convertWeiToSkill(nftPricesById[id]) === '0'"
                       @click="selectedNftId = id; canPurchase && purchaseNft();"
                       variant="primary"
                       v-bind:class="[!canPurchase ? 'disabled-button' : '']"
                       class="gtag-link-others" tagname="confirm_purchase">
-                      Purchase <b-icon-question-circle v-if="!canPurchase"
+                      {{ convertWeiToSkill(nftPricesById[id]) !== '0' ? 'Purchase' : 'Sold' }} <b-icon-question-circle v-if="!canPurchase"
                       v-tooltip.bottom="'You already have max amount of characters (4).'"/>
                     </b-button>
                   </div>
+                </template>
+
+                <template #sold="{ character: { id } }">
+                  <div class="sold" v-if="nftPricesById[id] && convertWeiToSkill(nftPricesById[id]) === '0'"><span>sold</span></div>
                 </template>
 
               </character-list>
