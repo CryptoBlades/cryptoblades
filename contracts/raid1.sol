ad5d2219ef7dd019d3f2194764241668626c4b82
PoshDan <karsai.daniel.93@gmail.com>
2021-07-24 09:52:35 +0200

    WIP commit, it's coming together

--- contracts/raid1.sol
+++ contracts/raid1.sol
@@ -19,9 +19,8 @@ contract Raid1 is Initializable, MultiAccessUpgradeable {
 
     using ABDKMath64x64 for int128;
     using ABDKMath64x64 for uint256;
-
-    bool public completed;
-    uint256 public expectedFinishTime; // not a guarantee since we don't automate this (atm)
+    
+    bytes32 public constant GAME_ADMIN = keccak256("GAME_ADMIN");
 
     CryptoBlades public game;
     Characters public characters;
@@ -32,37 +31,60 @@ contract Raid1 is Initializable, MultiAccessUpgradeable {
         uint256 charID;
         uint256 wepID;
         uint24 power;
+        uint24 traitsCWS;//char trait, wep trait, wep statpattern
     }
 
     uint64 public staminaDrain;
-    uint8 bossTrait; // set manually for now
+    int128 public joinCost;
 
-    uint256 totalPower;
-
-    int128 joinCost;
-
-    uint256 raidIndex;
-    mapping(uint256 => bool) public raidCompleted;
+    uint256 public raidIndex;
+    // all keys are raidIndex
+    mapping(uint256 => bool) public raidCompleted; // may be unnecessary if you do completionseed != 0
+    mapping(uint256 => uint256) public raidEndTime; // check == 0 to see if it started yet
     mapping(uint256 => uint256) public raidCompletionSeeds;
     mapping(uint256 => uint8) public raidBossTrait;
     mapping(uint256 => uint256) public raidBossPower;
+    mapping(uint256 => uint256) public raidPlayerPower;
+    mapping(uint256 => Raider[]) public raidParticipants;
+    mapping(uint256 => mapping(uint256/*address*/ => uint256[])) public raidParticipantIndices;
+    mapping(uint256 => mapping(uint256 => bool)) public raidRewardClaimed;
 
-    event RewardClaimed(address addr);
+    event RaidStarted(uint256 indexed raidIndex, uint8 bossTrait, uint256 bossPower, uint256 endTime);
+    event RaidJoined(uint256 indexed raidIndex,
+        address indexed user,
+        uint256 indexed character,
+        uint256 indexed weapon,
+        uint256 skillPaid);
+    event RaidCompleted(uint256 indexed raidIndex, bool victory, uint256 bossPower, uint256 playerPower);
+    
+    // reward specific events for analytics
+    event RewardClaimed(uint256 indexed raidIndex, address indexed user);
+    event RewardedDustLB(uint256 indexed raidIndex, address indexed user, uint256 amount);
+    event RewardedDust4B(uint256 indexed raidIndex, address indexed user, uint256 amount);
+    event RewardedDust5B(uint256 indexed raidIndex, address indexed user, uint256 amount);
+    event RewardedWeapon(uint256 indexed raidIndex, address indexed user, uint8 stars, uint256 indexed tokenID);
+    event RewardedJunk(uint256 indexed raidIndex, address indexed user, uint8 stars, uint256 indexed tokenID);
+    event RewardedTrinket(uint256 indexed raidIndex, address indexed user, uint8 stars, uint256 indexed tokenID);
+    event RewardedKeyBox(uint256 indexed raidIndex, address indexed user, uint256 indexed tokenID);
 
     function initialize(address gameContract) public initializer {
         
         MultiAccessUpgradeable.initialize();
 
-        grantAccess(gameContract);
         game = CryptoBlades(gameContract);
         characters = Characters(game.characters());
         weapons = Weapons(game.weapons());
 
-        staminaDrain = 16 * 60 * 60;
+        staminaDrain = 57600; // 16 hours
         joinCost = ABDKMath64x64.fromUInt(10);// 10 usd
     }
 
-    function addRaider(uint256 characterID, uint256 weaponID) public override {
+    modifier restricted() {
+        require(hasRole(GAME_ADMIN, msg.sender), "Not game admin");
+        _;
+    }
+
+    function joinRaid(uint256 characterID, uint256 weaponID) public {
         require(characters.ownerOf(characterID) == msg.sender);
         require(weapons.ownerOf(weaponID) == msg.sender);
         require(characters.getStaminaPoints(characterID) > 0, "You cannot join with 0 character stamina");
@@ -78,63 +100,46 @@ contract Raid1 is Initializable, MultiAccessUpgradeable {
         
         weapons.drainDurability(weaponID, staminaDrain, true);
 
-        uint24 power = game.getPlayerPower(basePowerLevel, weaponMultFight, weaponBonusPower);
-        //game.getPlayerPowerRoll(playerFightPower,traitsCWE,seed);
+        uint24 power = getPlayerFinalPower(
+            game.getPlayerPower(basePowerLevel, weaponMultFight, weaponBonusPower),
+            charTrait,
+            raidBossTrait[raidIndex]
+        );
         totalPower += power;
-        raiders.push(Raider(uint256(msg.sender), characterID, weaponID, power));
+        uint8 wepStatPattern = weapons.getStatPattern(weaponID);
 
-        emit RaiderJoined(msg.sender, characterID, weaponID, power);
+        raidParticipants[raidIndex].push(Raider(uint256(msg.sender), characterID, weaponID, power, traitCWS));
+
+        emit RaidJoined(raidIndex,
+            msg.sender,
+            characterID,
+            weaponID,
+            game.usdToSkill(joinCost));
         
         // maybe "restrict" this pay method and do the join from game?
+        // it woul help simplify gas usage analytics and skill approvals
         // we may need the "requestPayFromPlayer" modifier
+        // game contract would need a raid contract link
         game.payContract(msg.sender, joinCost);
     }
 
-    function completeRaid(uint256 seed) public override restricted {
-        require(completed == false, "Raid already completed, run reset first");
-        completed = true;
+    function getPlayerFinalPower(uint24 playerPower, uint8 charTrait, uint8 bossTrait) returns(uint24) {
+        if(game.isTraitEffectiveAgainst(charTrait, bossTrait))
+            return uint24(ABDKMath64x64.divu(1075,1000).mulu(uint256(playerPower)));
+        return playerPower;
+    }
 
-        raidCompletionSeeds[currentRaidIndex++] = seed;
-
-        // TODO rewards, on claim (including xp)
-        /*for(uint i = 0; i < raiders.length; i++) {
-            emit XpReward(address(raiders[i].owner), raiders[i].charID, 96);
-            characters.gainXp(raiders[i].charID, 96);
-        }*/
-        /*for(uint i = 0; i < raiders.length; i++) {
-            Raider memory r = raiders[i];
-
-            int128 powerPercentage = uint256(r.power).divu(totalPower);
-            uint256 payout = powerPercentage.mulu(bounty);
-            game.payPlayerConverted(address(r.owner), payout);
-            emit SkillWinner(address(r.owner), payout);
-        }*/
-
-        for(uint i = 0; i < weaponDrops.length; i++) {
-            Raider memory r = raiders[RandomUtil.randomSeededMinMax(0, raiders.length-1, RandomUtil.combineSeeds(seed,i))];
-            game.approveContractWeaponFor(weaponDrops[i], address(this));
-            weapons.transferFrom(address(game), address(r.owner), weaponDrops[i]);
-            emit WeaponWinner(address(r.owner), weaponDrops[i]);
-        }
-
+    function completeRaid(uint256 seed) public restricted {
+        
         emit RaidCompleted();
-    }
-
-    function reset() public override restricted {
-        totalPower = 0;
-        super.reset();
-    }
-
-    function setBossTrait(uint8 trait) public restricted {
-        bossTrait = trait;
-    }
-
-    function getTotalPower() public view returns(uint256) {
-        return totalPower;
     }
 
     function claimReward(uint256 raidIndex) public {
         //characters.processRaidParticipation(charID, victory, xp)
+    }
+
+    function getTotalPower() public view returns(uint256) {
+        return totalPower;
     }
 
     function setStaminaDrainSeconds(uint64 secs) public restricted {
