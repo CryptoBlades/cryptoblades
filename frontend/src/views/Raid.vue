802c516569181a51b2c754bcc22bb5d1a7cad1f9
newb23 <newb23@gmail.com>
2021-07-27 01:32:06 -0600

    Adds additional framework for Raid.vue
    Compiles but un-tested. Game contract needs to be revisited for performance/size.

--- frontend/src/views/Raid.vue
+++ frontend/src/views/Raid.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="body main-font">
-
     <div class="left-side fill-space raid-info">
-
       <div class="left-side">
         <div class="finish">
           <span class="title">Finishes on</span>
-          <br>
-          May 4th, 2021
+          <br />
+          {{expectedFinishTime}}
         </div>
 
         <div class="raiders">
-          <span class="title">Raiders</span> 69
-          <br>
-          <span class="title">Total Power</span> 69k
-          <br>
-          <span class="title">Bounty</span> 10 SKILL
+          <span class="title">Raiders</span> {{raiderCount}}
+          <br />
+          <span class="title">Total Power</span> {{totalPower}}
+          <br />
+          <span class="title">Bounty</span> {{bounty}}
         </div>
 
         <div class="drops">
           <span class="title">Drops</span>
-          <br>
-          items
+          <br />
+          {{weaponDrops}}
         </div>
       </div>
 
@@ -31,12 +29,12 @@
         </div>
 
         <div class="image">
-          <img src="../assets/DragonFlyIdle_512.gif">
+          <img src="../assets/DragonFlyIdle_512.gif" />
         </div>
 
         <div class="about-raid">
           <span class="title">Raid Name</span>
-          <br>
+          <br />
           <span class="lightning">Lightning</span>
         </div>
       </div>
@@ -59,7 +57,7 @@
         </div>
 
         <div class="power">
-          <div class="left-side">Character Power: 10000</div>
+          <div class="left-side">Character Power: {{10000}}</div>
           <div class="right-side">Weapon Multiplier: x1.23</div>
         </div>
 
@@ -68,38 +66,61 @@
         </div>
 
         <div class="action">
-          <big-button
-            class="button"
-            mainText="Sign up"
-          />
+          <big-button class="button" mainText="Sign up" />
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-
+import { mapActions, mapGetters,mapState } from 'vuex';
 import CharacterList from '../components/smart/CharacterList.vue';
 import WeaponGrid from '../components/smart/WeaponGrid.vue';
 import BigButton from '../components/BigButton.vue';
+import WeaponIcon from '../components/WeaponIcon.vue';
 
 export default {
   computed: {
-
+    ...mapState(['currentCharacterId']),
     ...mapGetters([
       'ownCharacters',
-      'ownWeapons'
+      'ownWeapons',
+      'ownCharacters',
+      'currentCharacter',
+      'currentCharacterStamina',
+      'getWeaponDurability',
+      'fightGasOffset',
+      'fightBaseline',
     ]),
+
+    selections() {
+      return [this.currentCharacterId, this.selectedWeaponId];
+    },
   },
 
-  props: {
-  },
+  props: {},
 
   methods: {
-    ...mapActions(['fetchRaidData', 'fetchOwnedCharacterRaidStatus'])
+    ...mapActions(['fetchRaidData', 'fetchOwnedCharacterRaidStatus']),
+
+    weaponHasDurabilit(id) {
+      return this.getWeaponDurability(id) > 0;
+    },
+
+    async onClickEncounter() {
+      if (this.selectedWeaponId === null || this.currentCharacterId === null) {
+        return;
+      }
+    },
+  },
+
+  watch: {
+    async selections([characterId, weaponId]) {
+      if (!this.ownWeapons.find((weapon) => weapon.id === weaponId)) {
+        this.selectedWeaponId = null;
+      }
+    },
   },
 
   async mounted() {
@@ -109,7 +130,8 @@ export default {
   components: {
     BigButton,
     CharacterList,
-    WeaponGrid
+    WeaponGrid,
+    WeaponIcon,
   },
 };
 </script>
@@ -133,19 +155,22 @@ export default {
   padding-top: 3em;
 }
 
-.left-side, .right-side {
+.left-side,
+.right-side {
   flex: 1;
   display: flex;
   flex-direction: column;
 }
 
-.chooser, .power {
+.chooser,
+.power {
   flex: 1;
   display: flex;
   flex-direction: row;
 }
 
-.chooser .left-side, .chooser .right-side {
+.chooser .left-side,
+.chooser .right-side {
   max-height: 300px;
   overflow-y: auto;
 }
@@ -154,17 +179,21 @@ export default {
   flex-direction: row;
 }
 
-.raiders, .drops {
+.raiders,
+.drops {
   margin-top: 1em;
 }
 
-.raid-boss, .raid-signup {
+.raid-boss,
+.raid-signup {
   justify-content: space-between;
 }
 
-.warning, .power, .total-power, .action {
+.warning,
+.power,
+.total-power,
+.action {
   text-align: center;
   margin-top: 0.5em;
 }
-
 </style>
