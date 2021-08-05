aa63e4848cc83b5b542830a113effe885fff8db8
Raymond Hammarling <raymond@hammarlings.se>
2021-08-05 08:36:33 +0200

    Deploy other upgraded contracts as well

--- migrations/61_TBD_shields.js
+++ migrations/61_TBD_shields.js
@@ -1,17 +1,23 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
+const CryptoBlades = artifacts.require("CryptoBlades");
 const Blacksmith = artifacts.require("Blacksmith");
 const Shields = artifacts.require("Shields");
 const NFTMarket = artifacts.require("NFTMarket");
+const Promos = artifacts.require("Promos");
 
 module.exports = async function (deployer, network, accounts) {
+  await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
+  await upgradeProxy(Promos.address, Promos, { deployer });
+
   const shields = await deployProxy(Shields, [], { deployer });
 
-  const blacksmith = await Blacksmith.deployed();
+  const blacksmith = await upgradeProxy(Blacksmith.address, Blacksmith, { deployer });
   await blacksmith.migrateTo_TBD(shields.address);
+
   const shields_GAME_ADMIN = await shields.GAME_ADMIN();
   await shields.grantRole(shields_GAME_ADMIN, blacksmith.address);
 
   const market = await NFTMarket.deployed();
-  market.allowToken(shields.address);
+  await market.allowToken(shields.address);
 };
