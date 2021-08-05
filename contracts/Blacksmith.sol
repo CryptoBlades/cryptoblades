b830268b1e94eb31deda8d6fd384351922b4b007
PoshDan <karsai.daniel.93@gmail.com>
2021-08-05 23:35:39 +0200

    added erc20 recover ability to blacksmith

--- contracts/Blacksmith.sol
+++ contracts/Blacksmith.sol
@@ -2,12 +2,15 @@ pragma solidity ^0.6.5;
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./interfaces/IRandoms.sol";
 import "./shields.sol";
 import "./weapons.sol";
 import "./cryptoblades.sol";
 
 contract Blacksmith is Initializable, AccessControlUpgradeable {
+    using SafeERC20 for IERC20;
     /* ========== CONSTANTS ========== */
 
     bytes32 public constant GAME = keccak256("GAME");
@@ -53,6 +56,12 @@ contract Blacksmith is Initializable, AccessControlUpgradeable {
     /* ========== VIEWS ========== */
 
     /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function recoverToken(address tokenAddress, uint256 amount) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Not admin");
+
+        IERC20(tokenAddress).safeTransfer(msg.sender, amount);
+    }
 
     function spendTicket(uint32 _num) external {
         require(_num > 0);
