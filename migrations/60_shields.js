4ebec548baf49328e850191a225c64baccd758bc
Raymond Hammarling <raymond@hammarlings.se>
2021-08-05 08:06:43 +0200

    Allow Shield NFT to be traded on market

--- migrations/60_shields.js
+++ migrations/60_shields.js
@@ -2,12 +2,16 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 const Blacksmith = artifacts.require("Blacksmith");
 const Shields = artifacts.require("Shields");
+const NFTMarket = artifacts.require("NFTMarket");
 
 module.exports = async function (deployer, network, accounts) {
   const shields = await deployProxy(Shields, [], { deployer });
-  
+
   const blacksmith = await Blacksmith.deployed();
   await blacksmith.migrateTo_TBD(shields.address);
   const shields_GAME_ADMIN = await shields.GAME_ADMIN();
   await shields.grantRole(shields_GAME_ADMIN, blacksmith.address);
+
+  const market = await NFTMarket.deployed();
+  market.allowToken(shields.address);
 };
