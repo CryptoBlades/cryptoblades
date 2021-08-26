2d6bb0550383535e251c013da9a8a311b9ff6c95
Krzysztof Borowiecki <krzysborowiecki97@gmail.com>
2021-08-26 19:34:57 +0200

    CombatResults.vue new design

--- frontend/src/components/CombatResults.vue
+++ frontend/src/components/CombatResults.vue
@@ -8,18 +8,36 @@
     </div>
   <div class="results-panel">
     <div class="float-right">
-      <h1 class="text-center outcome">{{formattedVictory}}</h1>
-      <p> You gained:  <span class="text-success">{{formattedXpGain}}</span>
-        <br/>
-        You earned: <span class="text-success" v-tooltip="formattedSkillTooltip">{{formattedSkill}}</span>
-        <Hint text="SKILL earned is based on gas costs of the network plus a factor of your power" />
-        <br/>
-        You spent ~ <span class="text-danger">{{fightResults.bnbGasUsed}}</span> BNB on gas fees
-      </p>
-      <p>You rolled: <span class="text-success">{{fightResults.playerRoll}}</span>
-        <br/>
-        Enemy rolled: <span class="text-danger">{{fightResults.enemyRoll}}</span>
-      </p>
+      <h1 class="text-left outcome">{{ formattedOutcome }}</h1>
+      <b-container v-if="fightResults.isVictory">
+        <b-row>
+          <b-col class="text-left no-padding">
+            <h4>You earned:</h4>
+          </b-col>
+          <b-col class="text-center no-padding">
+            <h4>
+              {{formattedUsd}}
+              <Hint text="SKILL earned is based on gas costs of the network plus a factor of your power" />
+            </h4>
+            <h6 class="formatted-skill">{{formattedSkill}}</h6>
+            <h5>{{formattedXpGain}}</h5>
+          </b-col>
+        </b-row>
+      </b-container>
+      <h6 class="text-left gas-spent">You spent {{fightResults.bnbGasUsed}} BNB on gas fees </h6>
+      <img src="../assets/divider4.png" class="expander-divider">
+      <b-container>
+        <b-row>
+          <b-col class="text-left no-padding">
+            <h5 class="no-margin">You rolled:</h5>
+            <h5 class="no-margin">Enemy rolled:</h5>
+          </b-col>
+          <b-col class="text-center no-padding">
+            <h5 class="no-margin">{{fightResults.playerRoll}}</h5>
+            <h5 class="no-margin">{{fightResults.enemyRoll}}</h5>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
   </div>
 
@@ -43,6 +61,7 @@ import Vue from 'vue';
 import { toBN, fromWeiEther } from '../utils/common';
 import Hint from '../components/Hint.vue';
 import {PropType} from 'vue/types/options';
+import axios from 'axios';
 
 interface CombatResult {
   isVictory: boolean;
@@ -60,29 +79,45 @@ export default Vue.extend({
       default() {
         return {} as CombatResult;
       },
-    },
+    }
+  },
+
+  data() {
+    return {
+      skillPrice: 0,
+    };
   },
 
   computed: {
-    formattedSkill() {
-      const skillBalance = fromWeiEther(this.fightResults.skillGain);
-      return `${toBN(skillBalance).toFixed(6)} SKILL`;
+    formattedOutcome(): string {
+      return `You ${this.fightResults.isVictory ? 'won' : 'lost'} the fight!`;
     },
-    formattedSkillTooltip() {
+    formattedUsd(): string {
+      return `$${(this.calculateSkillPriceInUsd()).toFixed(2)}`;
+    },
+    formattedSkill(): string {
+      return `(${toBN(fromWeiEther(this.fightResults.skillGain)).toFixed(6)} SKILL)`;
+    },
+    formattedSkillTooltip(): string {
       return fromWeiEther(this.fightResults.skillGain)+' SKILL';
     },
-    formattedVictory() {
-      return this.fightResults.isVictory ? 'VICTORY' : 'DEFEAT';
-    },
-    formattedXpGain() {
+    formattedXpGain(): string {
       return this.fightResults.xpGain + ' xp';
     }
   },
 
-  mounted() {
+  methods: {
+    async fetchPrices(): Promise<void> {
+      const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=cryptoblades,binancecoin&vs_currencies=usd');
+      this.skillPrice = response.data?.cryptoblades.usd;
+    },
+    calculateSkillPriceInUsd(): number {
+      return fromWeiEther(this.fightResults.skillGain) as unknown as number * this.skillPrice as unknown as number;
+    }
   },
 
-  methods: {
+  async mounted() {
+    await this.fetchPrices();
   },
 
   components: {
@@ -105,18 +140,27 @@ export default Vue.extend({
   font-weight: bold;
   padding: 0.1em;
   margin-top: 0.25em;
+  margin-bottom: 0;
 }
-.victory {
-  color:greenyellow;
+.expander-divider {
+  width: 100%;
+  position: relative;
+  height: 0.2em;
 }
-.loss {
-  color: red;
+.formatted-skill {
+  margin-top: -10px;
+  margin-bottom: 0;
+  font-size: 0.8em;
 }
-.roll {
-  font-size: 1.25em;
+.gas-spent {
+  font-size: 1em;
+  margin-bottom: 0;
 }
-.reward {
-  font-size: 1.5em;
+.no-padding {
+  padding: 0 !important;
+}
+.no-margin {
+  margin: 0;
 }
 .crypto-warrior-image {
   max-width: 13em;
