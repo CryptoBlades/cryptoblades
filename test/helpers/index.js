be931820b8dbb8c9362579939df1f65981790e75
Rafael Vidaurre <rafael@vidaurre.io>
2021-08-30 16:56:29 +0200

    wip: add more tests

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -2,6 +2,7 @@ const { expectEvent, time } = require('@openzeppelin/test-helpers');
 
 const Characters = artifacts.require('Characters');
 const Weapons = artifacts.require('Weapons');
+const Shields = artifacts.require('Shields');
 
 /**
  * @param {string} receiver address of who receives the character
@@ -36,7 +37,24 @@ async function createWeapon(receiver, seed, context = {}) {
   return newWeaponEvt.args.weapon;
 }
 
+/**
+ * @param {string} receiver address of who receives the shield
+ * @param {string} seed seed for generating the shield
+ * @param {Weapons} [context.shields] instance of the Shields contract
+ * 
+ * @return {Promise<string>} shieldID of the newly created weapon
+ */
+async function createShield(receiver, seed, context = {}) {
+  const shields = context.shields || await Shields.deployed();
+  const { tx: mintShieldTx } = await shields.mint(receiver, seed);
+
+  const newShieldEvt = await expectEvent.inTransaction(mintShieldTx, Shields, 'NewShield', { minter: receiver });
+  await time.advanceBlock();
+  return newShieldEvt.args.tokenID;
+}
+
 module.exports = {
   createCharacter,
   createWeapon,
+  createShield
 };
--- test/pvp-arena.js
+++ test/pvp-arena.js
@@ -1,12 +1,13 @@
 const web3 = require('web3');
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const helpers = require('./helpers');
 
 const SkillToken = artifacts.require('SkillToken');
 const Characters = artifacts.require('Characters');
 const Weapons = artifacts.require('Weapons');
+const Shields = artifacts.require('Shields');
 const PvpArena = artifacts.require('PvpArena');
 const BasicPriceOracle = artifacts.require('BasicPriceOracle');
-const NFTMarket = artifacts.require('NFTMarket');
 
 contract('PvpArena', accounts => {
   let pvpArena, characters, weapons;
@@ -14,15 +15,15 @@ contract('PvpArena', accounts => {
     skillToken = await SkillToken.deployed();
     characters = await Characters.deployed();
     weapons = await Weapons.deployed();
+    shields = await Shields.deployed();
     pvpArena = await PvpArena.deployed();
     priceOracle = await BasicPriceOracle.deployed();
-    market = await NFTMarket.deployed();
 
     // TODO: Check if this is right
     await characters.grantRole(await characters.GAME_ADMIN(), accounts[0]);
     await characters.grantRole(await characters.NO_OWNED_LIMIT(), accounts[1]);
     await weapons.grantRole(await weapons.GAME_ADMIN(), accounts[0]);
-    await market.grantRole(await market.GAME_ADMIN(), accounts[0]);
+    await shields.grantRole(await shields.GAME_ADMIN(), accounts[0]);
 
     await skillToken.transferFrom(skillToken.address, accounts[1], web3.utils.toWei('10', 'ether'));
     
@@ -43,8 +44,8 @@ contract('PvpArena', accounts => {
     });
 
     it('should return the right arena tier', async () => {
-      const character0arenaTier = await pvpArena.getArenaTier(character0Id);
-      const character1arenaTier = await pvpArena.getArenaTier(character1Id);
+      const character0arenaTier = await pvpArena.getArenaTier(character0Id, { from: accounts[1] });
+      const character1arenaTier = await pvpArena.getArenaTier(character1Id, { from: accounts[1] });
       expect(character0arenaTier.toString()).to.equal('0');
       expect(character1arenaTier.toString()).to.equal('1');
     });
@@ -52,25 +53,81 @@ contract('PvpArena', accounts => {
 
   describe('#enterArena', () => {
     let characterId;
-    let character;
-    let enterArenaReceipt;
     let weaponId;
-    let weapon;
+    let shieldId;
+    let cost;
+
+    beforeEach(async () => {
+      weaponId = await helpers.createWeapon(accounts[1], '123', { weapons });
+      shieldId = await helpers.createShield(accounts[1], '123', { shields });
+      characterId = await helpers.createCharacter(accounts[1], '123', { characters });
+    });
 
     describe('happy path', async () => {
-      beforeEach(async () => {
-        weaponId = await helpers.createWeapon(accounts[0], '123', { weapons });
-        weapon = await weapons.get(weaponId);
-        characterId = await helpers.createCharacter(accounts[0], '123', { characters });
-        character = await characters.get(characterId);
+      let enterArenaReceipt;
 
-        const cost = await pvpArena.getEntryCost(characterId);
-        await skillToken.approve(pvpArena.address, cost);
-        enterArenaReceipt = await pvpArena.enterArena(characterId, weaponId);
+      beforeEach(async () => {
+        cost = await pvpArena.getEntryCost(characterId, { from: accounts[1] });
+        await skillToken.approve(pvpArena.address, cost, { from: accounts[1] });
+        enterArenaReceipt = await pvpArena.enterArena(characterId, weaponId, { from: accounts[1] });
       });
 
-      it('should lock the entry cost', async () => {
-        const cost = await pvpArena.getEntryCost(characterId);
+      it('should lock the entry cost');
+
+      it('should add the character with its weapon and shield to the fighters list');
+    });
+
+    describe('character unavailable', async () => {
+      let weapon2Id;
+
+      beforeEach(async () => {
+        weapon2Id = await helpers.createWeapon(accounts[1], '123', { weapons });
+
+        cost = await pvpArena.getEntryCost(characterId);
+        await skillToken.approve(pvpArena.address, cost, { from: accounts[1] });
+        await pvpArena.enterArena(characterId, weaponId, { from: accounts[1] });
+      });
+
+      it('should revert', async () => {
+        await expectRevert(pvpArena.enterArena(characterId, weapon2Id, { from: accounts[1] }));
+      });
+    });
+
+    describe('weapon unavailable', async () => {
+      beforeEach(async () => {
+        character2Id = await helpers.createCharacter(accounts[1], '443', { characters });
+
+        cost = await pvpArena.getEntryCost(characterId, { from: accounts[1] });
+        await skillToken.approve(pvpArena.address, cost, { from: accounts[1] });
+        await pvpArena.enterArena(characterId, weaponId, { from: accounts[1] });
+      });
+
+      it('should revert', async () => {
+        await expectRevert(pvpArena.enterArena(character2Id, weaponId, { from: accounts[1] })); 
+      });
+    });
+
+    describe('shield unavailable', () => {
+      beforeEach(async () => {
+        character2Id = await helpers.createCharacter(accounts[1], '152', { characters });
+        weapon2Id = await helpers.createWeapon(accounts[1], '123', { weapons });
+        shieldId = await helpers.createShield(accounts[1], '446', { shields });
+
+        cost = await pvpArena.getEntryCost(characterId, { from: accounts[1] });
+        await skillToken.approve(pvpArena.address, cost, { from: accounts[1] })
+        // await pvpArena.enterArena(characterId, weaponId, shieldId, { from: accounts[1] })
+      });
+
+      it('should revert', async () => {
+        // await expectRevert(pvpArena.enterArena(character2Id, weapon2Id, shieldId, { from: accounts[1] }));
+      })
+    });
+
+    describe('character is not sender\'s', () => {
+      beforeEach(async () => {
+        cost = await pvpArena.getEntryCost(characterId, { from: accounts[1] });
+        await skillToken.approve(pvpArena.address, cost, { from: accounts[1] });
+        await pvpArena.enterArena(characterId, weaponId, { from: accounts[1] });
       });
     });
   });
