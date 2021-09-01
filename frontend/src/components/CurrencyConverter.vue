cb8ceb0c5afd590f01707226ab7ad44943f16a44
Krzysztof Borowiecki <krzysborowiecki97@gmail.com>
2021-09-01 18:24:49 +0200

    raid-and-weapon-ui-fixes

--- frontend/src/components/CurrencyConverter.vue
+++ frontend/src/components/CurrencyConverter.vue
@@ -8,34 +8,34 @@
 </template>
 
 <script lang="ts">
-
 import Vue from 'vue';
-import {toBN} from '@/utils/common';
+import { toBN } from '@/utils/common';
 import Events from '@/events';
-import {mapState} from 'vuex';
+import { mapState } from 'vuex';
+import BigNumber from 'bignumber.js';
 
 export default Vue.extend({
   props: {
     skill: {
       type: String,
-      default: '0'
+      default: '0',
     },
     skillMinDecimals: {
       type: Number,
-      default: 2
+      default: 2,
     },
     skillMaxDecimals: {
       type: Number,
-      default: 4
+      default: 4,
     },
     usdDecimals: {
       type: Number,
-      default: 2
+      default: 2,
     },
     showValueInSkillOnly: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data() {
     return {
@@ -45,7 +45,7 @@ export default Vue.extend({
   computed: {
     ...mapState(['skillPriceInUsd']),
     formattedUsd(): string {
-      return `$${(this.calculateSkillPriceInUsd()).toFixed(this.usdDecimals)}`;
+      return `$${this.calculateSkillPriceInUsd().toFixed(this.usdDecimals)}`;
     },
     formattedSkill(): string {
       return `${this.calculateSkillWithDecimals()} SKILL`;
@@ -53,23 +53,32 @@ export default Vue.extend({
   },
   methods: {
     calculateSkillPriceInUsd(): number {
-      return this.skill as unknown as number * this.skillPriceInUsd as unknown as number;
+      return ((this.skill as unknown as number) *
+        this.skillPriceInUsd) as unknown as number;
     },
     calculateSkillWithDecimals(): string {
       const parsedSkill = toBN(this.skill);
+      const decimalPlaces = this.countDecimalPlaces(parsedSkill);
 
-      if (parsedSkill < toBN(Math.pow(10, -this.skillMaxDecimals))) {
-        return '< ' + Math.pow(10, -this.skillMaxDecimals).toFixed(this.skillMaxDecimals);
+      if (this.skillMaxDecimals < decimalPlaces) {
+        return `~${parsedSkill.toFixed(this.skillMaxDecimals)}`;
       }
 
-      for (let i = this.skillMaxDecimals - 1; i >= this.skillMinDecimals; i--) {
-        if (parsedSkill < toBN(Math.pow(10, -i))) {
-          return parsedSkill.toFixed(i + 1);
-        }
+      if (
+        decimalPlaces > this.skillMinDecimals &&
+        decimalPlaces <= this.skillMaxDecimals
+      ) {
+        return parsedSkill.toString();
       }
 
       return parsedSkill.toFixed(this.skillMinDecimals);
     },
+
+    countDecimalPlaces(value: BigNumber) {
+      if (Math.floor(+value.valueOf()).toString() === value.valueOf()) return 0;
+      return value.toString().split('.')[1].length || 0;
+    },
+
     checkStorage() {
       this.showValueInUsd = localStorage.getItem('showSkillInUsd') === 'true';
     },
@@ -82,5 +91,4 @@ export default Vue.extend({
 </script>
 
 <style>
-
 </style>
