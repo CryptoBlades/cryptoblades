5134d73025876d6c2a274a2a24ebb07709129845
Kacper <kacpertokarczyk1@gmail.com>
2021-09-21 17:48:07 +0200

    update config, increase fight gas limit

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1625,7 +1625,7 @@ export function createStore(web3: Web3) {
             targetString,
             fightMultiplier
           )
-          .send({ from: state.defaultAccount, gas: '200000' });
+          .send({ from: state.defaultAccount, gas: '300000' });
 
         await dispatch('fetchTargets', { characterId, weaponId });
 
