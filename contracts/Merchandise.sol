7315b5e4217c300ee00e8b50f0a1eff85d703e73
PoshDan <karsai.daniel.93@gmail.com>
2021-11-02 02:38:56 +0100

    wip commit on merch store

--- contracts/Merchandise.sol
+++ contracts/Merchandise.sol
@@ -4,11 +4,14 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./cryptoblades.sol";
 
 contract Merchandise is Initializable, AccessControlUpgradeable {
     using SafeERC20 for IERC20;
-    
+    using SafeMath for uint256;
+    using SafeMath for uint64;
+
     /* ========== CONSTANTS ========== */
 
     bytes32 public constant GAME_ADMIN = keccak256("GAME_ADMIN");
@@ -17,6 +20,8 @@ contract Merchandise is Initializable, AccessControlUpgradeable {
     uint256 public constant STATUS_CANCELLED = 1; // cancelled before processing (refunded)
     uint256 public constant STATUS_PROCESSED = 2; // bot has submitted the order to the shop
     uint256 public constant STATUS_ERROR = 3;
+
+    uint256 public constant LINK_SKILL_TOKEN = 1;
 
     uint256 public constant VAR_ACCEPTING_ORDERS = 1; // can orders be placed?
     uint256 public constant VAR_TRACK_INCOME = 2; // will income go to payouts?
@@ -29,7 +34,7 @@ contract Merchandise is Initializable, AccessControlUpgradeable {
     mapping(uint256 => address) public links;
     mapping(uint256 => uint256) public vars;
 
-    mapping(uint256 => uint256) public itemPrices;
+    mapping(uint256 => uint256) public itemPrices; // stored in USD cents
 
     /* ========== INITIALIZERS AND MIGRATORS ========== */
 
@@ -43,8 +48,6 @@ contract Merchandise is Initializable, AccessControlUpgradeable {
 
         game = _game;
         skillOracle = _skillOracle;
-
-        itemPrices[1] = 199;// TEMP TEST, ERASE
     }
 
     /* ========== VIEWS ========== */
@@ -52,15 +55,50 @@ contract Merchandise is Initializable, AccessControlUpgradeable {
     function getPriceOfItem(uint256 item) public view returns (uint256) {
         return itemPrices[item] * skillOracle.currentPrice() / 100;
     }
+
+    function getPriceOfBasket(uint256[] memory items, uint8[] memory amounts) returns (uint256) {
+        uint256 sumCents = 0;
+        for(uint i = 0; i < items.length; i++)
+            sumCents = sumCents.add(itemPrices[items[i]]);
+        return sumCents.mul(skillOracle.currentPrice()) / 100;
+    }
     
     /* ========== MUTATIVE FUNCTIONS ========== */
 
-    function recoverToken(address tokenAddress, uint256 amount) public isAdmin {
+    function recoverToken(address tokenAddress, uint256 amount) external isAdmin {
         IERC20(tokenAddress).safeTransfer(msg.sender, amount);
     }
 
-    function setSkillOracle(IPriceOracle newOracle) public isAdmin {
+    function setLink(uint256 linkId, address linkAddress) external isAdmin {
+        links[linkId] = linkAddress;
+    }
+
+    function setSkillOracle(IPriceOracle newOracle) external isAdmin {
         skillOracle = newOracle;
+    }
+
+    function setItemPrice(uint256 item, uint256 usdCents) external restricted {
+        itemPrices[item] = usdCents;
+    }
+
+    function setItemPrices(uint256[] calldata items, uint256[] prices) external restricted {
+        for(uint i = 0; i < items.length; i++)
+            itemPrices[items[i]] = prices[i];
+    }
+
+    function setItemsToPrice(uint256[] calldata items, uint256 price) external restricted {
+        for(uint i = 0; i < items.length; i++)
+            itemPrices[items[i]] = prices[i];
+    }
+
+    function placeOrder(uint256[] calldata items, uint8[] memory amounts) external returns (uint256) {
+        IERC20(links[LINK_SKILL_TOKEN])
+            .safeTransferFrom(msg.sender, address(this), getPriceOfBasket(items,amounts));
+        return 0; // order ID
+    }
+
+    function cancelOrder(uint256 orderId) external {
+
     }
 
     /* ========== MODIFIERS ========== */
