b0f9b5e93057f67fb8b2e014ec370b1a4107f6db
Moussa Khalil <moussakhalil1990@gmail.com>
2021-11-11 20:59:45 +0200

    781-nft-bridge logs for bot and tracking

---
+++ migrations/98_nftstorage_logging_for_bot.js
@@ -0,0 +1,7 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+const NFTStorage = artifacts.require("NFTStorage");
+
+module.exports = async function (deployer, network, accounts) {
+  await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
+};
