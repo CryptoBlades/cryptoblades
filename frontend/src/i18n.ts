3cb27db699bfdf789596f3128242d5bb0591cbd2
Kevin Unger <kevinunger98@gmail.com>
2021-11-12 17:54:20 +0100

    removed env locals

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -22,7 +22,7 @@ function loadLocaleMessages() {
 }
 
 export default new VueI18n({
-  locale: localStorage.getItem('language') || process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: localStorage.getItem('language') || 'en',
+  fallbackLocale: 'en',
   messages: loadLocaleMessages()
 });
