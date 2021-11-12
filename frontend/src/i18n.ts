fd22b9070f6a87b60b9e2c920f659469c1411f4e
Kevin Unger <kevinunger98@gmail.com>
2021-11-12 19:11:14 +0100

    if locale string is empty, use fallback string

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -3,6 +3,18 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
+//remove emtpy locale strings to use fallback locale strings
+const removeEmpty = (obj: { [x: string]: any; }): any =>
+  Object.keys(obj)
+    .filter((k: string) => obj[k] !== null && obj[k] !== undefined && obj[k] !== '') // Remove undef. and null and empty.string.
+    .reduce(
+      (newObj, k) =>
+        typeof obj[k] === 'object'
+          ? Object.assign(newObj, { [k]: removeEmpty(obj[k]) })
+          : Object.assign(newObj, { [k]: obj[k] }),
+      {},
+    );
+
 function loadLocaleMessages() {
   const locales = require.context(
     './locales',
@@ -14,7 +26,7 @@ function loadLocaleMessages() {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key);
+      messages[locale] = removeEmpty(locales(key));
     }
   });
   console.log(messages);
