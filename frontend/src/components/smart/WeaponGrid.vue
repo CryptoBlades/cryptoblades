96e667f83fc03ce3e553715d373b840054fa1da6
Kevin Unger <kevinunger98@gmail.com>
2021-11-12 19:14:59 +0100

    add string for 'Any'

--- frontend/src/components/smart/WeaponGrid.vue
+++ frontend/src/components/smart/WeaponGrid.vue
@@ -4,14 +4,14 @@
       <div class="col-sm-6 col-md-6 col-lg-6 mb-3">
         <strong>{{$t('weaponGrid.stars')}}</strong>
         <select class="form-control" v-model="starFilter" >
-          <option v-for="x in ['', 1, 2, 3, 4, 5]" :value="x" :key="x">{{ x || 'Any' }}</option>
+          <option v-for="x in ['', 1, 2, 3, 4, 5]" :value="x" :key="x">{{ x || $t('nftList.sorts.any') }}</option>
         </select>
       </div>
 
       <div class="col-sm-6 col-md-6 col-lg-6 mb-3">
         <strong>{{$t('weaponGrid.element')}}</strong>
         <select class="form-control" v-model="elementFilter" >
-          <option v-for="x in ['', 'Earth', 'Fire', 'Lightning', 'Water']" :value="x" :key="x">{{ x || 'Any' }}</option>
+          <option v-for="x in ['', 'Earth', 'Fire', 'Lightning', 'Water']" :value="x" :key="x">{{ x || $t('nftList.sorts.any') }}</option>
         </select>
       </div>
 
