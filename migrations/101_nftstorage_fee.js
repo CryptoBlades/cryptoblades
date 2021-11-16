1eea46c8252729805e20c7fefd347f0ee7f9e2f4
Kacper <kacpertokarczyk1@gmail.com>
2021-11-16 19:09:03 +0100

    Update 101_nftstorage_fee.js
    await calls

--- migrations/101_nftstorage_fee.js
+++ migrations/101_nftstorage_fee.js
@@ -7,9 +7,9 @@ module.exports = async function (deployer, network, accounts) {
    const cryptoBlades = await CryptoBlades.deployed();
    const storage = await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
    
-   storage.migrateTo_56837f7(cryptoBlades.address);
-   storage.setBridgeFee(web3.utils.toWei('0.1', 'ether'));
+   await storage.migrateTo_56837f7(cryptoBlades.address);
+   await storage.setBridgeFee(web3.utils.toWei('0.1', 'ether'));
    
    const game_GAME_ADMIN = await cryptoBlades.GAME_ADMIN();
-   cryptoBlades.grantRole(game_GAME_ADMIN, storage.address);
+   await cryptoBlades.grantRole(game_GAME_ADMIN, storage.address);
 };
