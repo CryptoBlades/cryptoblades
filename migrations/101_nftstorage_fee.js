5c785230482a5b31b2002c85646ca9d4c615b72c
Moussa Khalil <25177987+bilbolPrime@users.noreply.github.com>
2021-11-16 20:21:56 +0200

    781 nft bridge (#846)
    * 781-nft-bridge bridge fee
    
    * 781-nft-bridge bridge fee
    
    * proper disable request button
    
    * added loader to withdrawal + enhancements
    
    * warning for XP
    
    * add fee approval
    
    * +fee, +fee warning
    
    * remove unused contracts
    
    * added enums
    
    * disable condition
    
    * revert env
    
    * Update 101_nftstorage_fee.js
    
    await calls
    
    Co-authored-by: Kevin Unger <kevinunger98@gmail.com>
    Co-authored-by: Kacper <kacpertokarczyk1@gmail.com>

---
+++ migrations/101_nftstorage_fee.js
@@ -0,0 +1,15 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+const CryptoBlades = artifacts.require("CryptoBlades");
+const NFTStorage = artifacts.require("NFTStorage");
+
+module.exports = async function (deployer, network, accounts) {
+   const cryptoBlades = await CryptoBlades.deployed();
+   const storage = await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
+   
+   await storage.migrateTo_56837f7(cryptoBlades.address);
+   await storage.setBridgeFee(web3.utils.toWei('0.1', 'ether'));
+   
+   const game_GAME_ADMIN = await cryptoBlades.GAME_ADMIN();
+   await cryptoBlades.grantRole(game_GAME_ADMIN, storage.address);
+};
