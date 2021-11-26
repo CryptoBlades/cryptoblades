2411cc4a695db3c19f3ca7345c3b7ab73130b8aa
Kacper <kacpertokarczyk1@gmail.com>
2021-11-26 09:11:49 +0100

    update migration

--- migrations/104_treasury.js
+++ migrations/104_treasury.js
@@ -5,7 +5,7 @@ const CryptoBlades = artifacts.require("CryptoBlades");
 
 
 module.exports = async function (deployer, network, accounts) {
-  const game = await CryptoBlades.deployed();
+  const game = await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
   const GAME_ADMIN = await game.GAME_ADMIN();
 
   const treasury = await deployProxy(Treasury, [game.address], { deployer });
