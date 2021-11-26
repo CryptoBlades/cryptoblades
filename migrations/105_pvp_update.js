e5635eb8d5c5ff64d623b6c45a86d47fd5395a88
Diego Vidaurre <d.vidaurres@gmail.com>
2021-11-26 13:42:43 -0300

    fix migrations

--- migrations/105_pvp_update.js
+++
@@ -1,8 +0,0 @@
-const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-
-const PvpArena = artifacts.require("PvpArena");
-
-module.exports = async function (deployer) {
-  const pvpArena = await PvpArena.deployed();
-  await upgradeProxy(pvpArena.address, PvpArena, { deployer });
-};
