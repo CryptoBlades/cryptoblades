063fcb922c6c74f1bba5c353a4ee800addaeb862
Kacper <kacpertokarczyk1@gmail.com>
2021-11-27 18:48:20 +0100

    Update 104_king_staking.js
    Change owner to bsc deployer address

--- migrations/104_king_staking.js
+++ migrations/104_king_staking.js
@@ -4,8 +4,8 @@ const KingStakingRewardsUpgradeable = artifacts.require('KingStakingRewardsUpgra
 
 module.exports = async function (deployer, network) {
   if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
-    const ownerAddress = '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48';
-    const rewardDistributorAddress = '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48';
+    const ownerAddress = '0xa2bB660A6A3Bb5c74E36415ffe5D4862eFfc417A';
+    const rewardDistributorAddress = '0xa2bB660A6A3Bb5c74E36415ffe5D4862eFfc417A';
     const kingTokenAddress = '0x0ccd575bf9378c06f6dca82f8122f570769f00c2';
 
     await deployProxy(KingStakingRewardsUpgradeable, [ownerAddress, rewardDistributorAddress, kingTokenAddress, kingTokenAddress, 7 * 24 * 60 * 60], { deployer });
