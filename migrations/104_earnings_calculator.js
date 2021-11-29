deeffd029d7c0d3a7e0c921301c70d5dd743fbcf
PoshDan <karsai.daniel.93@gmail.com>
2021-11-29 21:03:09 +0100

    fixed migrations, adjusted merch contract

--- migrations/104_earnings_calculator.js
+++
@@ -1,7 +0,0 @@
-const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-
-const CryptoBlades = artifacts.require("CryptoBlades");
-
-module.exports = async function (deployer) {
-  await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
-};
--- migrations/104_treasury.js
+++ migrations/104_treasury.js
@@ -1,13 +1,14 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 const Treasury = artifacts.require("Treasury");
 const CryptoBlades = artifacts.require("CryptoBlades");
-
+const Merchandise = artifacts.require("Merchandise");
 
 module.exports = async function (deployer, network, accounts) {
   const game = await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
+  await upgradeProxy(Merchandise.address, Merchandise, { deployer });
   const GAME_ADMIN = await game.GAME_ADMIN();
 
   const treasury = await deployProxy(Treasury, [game.address], { deployer });
-  game.grantRole(GAME_ADMIN, treasury.address);
+  await game.grantRole(GAME_ADMIN, treasury.address);
 };
