9ce3da1b149c58fb225ec5a8916f506c5efac870
PoshDan <karsai.daniel.93@gmail.com>
2021-12-15 22:50:50 +0100

    updated migration file (112) for next deploy

--- migrations/112_merchandise_rework_and_duel_fix.js
+++ migrations/112_merchandise_rework_and_duel_fix.js
@@ -1,7 +1,9 @@
 const {upgradeProxy} = require('@openzeppelin/truffle-upgrades');
 
 const Merchandise = artifacts.require("Merchandise");
+const PvpArena = artifacts.require("PvpArena");
 
 module.exports = async function (deployer) {
   await upgradeProxy(Merchandise.address, Merchandise, {deployer});
+  await upgradeProxy(PvpArena.address, PvpArena, { deployer });
 };
