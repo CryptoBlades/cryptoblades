7771792cfb66912275ae941ad3708f0e522b06a4
PoshDan <karsai.daniel.93@gmail.com>
2021-12-20 17:00:09 +0100

    include required characters upgrade in migration

--- migrations/123_simple_quests.js
+++ migrations/123_simple_quests.js
@@ -1,9 +1,11 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 const SimpleQuests = artifacts.require("SimpleQuests");
+const Characters = artifacts.require("Characters");
 
 module.exports = async function (deployer, network, accounts) {
 	
   const SimpleQuests = await deployProxy(SimpleQuests, [], { deployer });
+  const characters = await upgradeProxy(Characters.address, Characters, { deployer });
 
 };
