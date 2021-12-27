b5368e279e927bb9913796a2f6de5cf6fc7593e0
Chriserus <krzysborowiecki97@gmail.com>
2021-12-27 17:21:01 +0100

    Add Quests route, cleanup router.ts

---
+++ frontend/src/views/Quests.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>Hello Quests</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>
