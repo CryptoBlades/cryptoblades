ddb8ed56e8b1428f4ecb71e729bfa488184e8bee
Chriserus <krzysborowiecki97@gmail.com>
2022-01-14 11:53:51 +0100

    Start using enums for visibility

--- contracts/SimpleQuests.sol
+++ contracts/SimpleQuests.sol
@@ -60,12 +60,17 @@ contract SimpleQuests is Initializable, AccessControlUpgradeable {
     struct Quest {
         uint256 id;
         uint8 tier;
-        uint32 requirementType;
-        uint32 rewardType;
+        RequirementType requirementType;
+        RewardType rewardType;
+        RewardRarity rewardRarity;
         uint256 requirementAmount;
         uint256 rewardAmount;
         uint256 reputationAmount;
     }
+
+    enum RequirementType{NONE, RAID}
+    enum RewardType{SWORD, JUNK}
+    enum RewardRarity{COMMON, UNCOMMON, RARE, EPIC, LEGENDARY}
 
     // have quests rarities on certain indexes (0 - common, 1 - uncommon, 2 - rare, 3 - epic)
     // or have quests in different arrays and join them on allQuestsList
@@ -106,8 +111,8 @@ contract SimpleQuests is Initializable, AccessControlUpgradeable {
 
     //Quest example: tier = 1, id = TBA, requirementType = 1, requirementAmount = 2, rewardType = 1, rewardAmount = 1, reputationAmount = 12
     // do 2 raids for 1x3* sword
-    function addNewQuest(uint32 index, uint8 tier, uint32 requirementType, uint32 rewardType, uint256 requirementAmount, uint256 rewardAmount, uint256 reputationAmount) public {
-        quests[tier][index] = Quest(0, tier, requirementType, rewardType, requirementAmount, rewardAmount, reputationAmount);
+    function addNewQuest(uint8 tier, RequirementType requirementType, RewardType rewardType, RewardRarity rewardRarity, uint256 requirementAmount, uint256 rewardAmount, uint256 reputationAmount) public {
+        quests[tier].push(Quest(0, tier, requirementType, rewardType, rewardRarity, requirementAmount, rewardAmount, reputationAmount));
     }
 
     function deleteQuest(uint8 tier, uint32 index) public {
@@ -130,7 +135,7 @@ contract SimpleQuests is Initializable, AccessControlUpgradeable {
         Quest memory quest = getNewQuest(0);
         characters.setNftVar(characterID, characters.NFTVAR_SIMPLEQUEST_ID(), quest.id);
         characters.setNftVar(characterID, characters.NFTVAR_SIMPLEQUEST_PROGRESS(), 0);
-        characters.setNftVar(characterID, characters.NFTVAR_SIMPLEQUEST_TYPE(), quest.requirementType);
+        characters.setNftVar(characterID, characters.NFTVAR_SIMPLEQUEST_TYPE(), uint256(quest.requirementType));
         emit QuestAssigned(quest.id, characterID);
         return quest.id;
     }
@@ -167,14 +172,14 @@ contract SimpleQuests is Initializable, AccessControlUpgradeable {
 
     function rewardQuest(uint256 questID, uint256 characterID) private {
         Quest memory quest = questList[questID];
-        if (quest.rewardType == 1) {// do a package, like, rewardType >= 1 && rewardType <= 4 and use the same mintWeaponWithStars based on that
+        if (quest.rewardType == RewardType.SWORD) {// do a package, like, rewardType >= 1 && rewardType <= 4 and use the same mintWeaponWithStars based on that
             // for now, rewardType = 1 means 3* sword
             // stars = 2 for 3* sword
             // random seed
             // random element = 100??
             for (uint8 i = 0; i < quest.rewardAmount; i++) {
                 uint256 seed = uint256(keccak256(abi.encodePacked(blockhash(block.number - 1))));
-                uint256 weaponID = weapons.mintWeaponWithStars(characters.ownerOf(characterID), 2, seed / 100, 100);
+                uint256 weaponID = weapons.mintWeaponWithStars(characters.ownerOf(characterID), uint256(quest.rewardRarity), seed / 100, 100);
             }
 
         }
@@ -187,13 +192,13 @@ contract SimpleQuests is Initializable, AccessControlUpgradeable {
     //        // it will complete by the randoms bot/contract automatically
     //    }
 
-    function submitProgress(uint256 characterID, uint256 amount) external questsEnabled {
-        uint256[] memory questData = getCharacterQuestData(characterID);
-        assertOnQuest(questData);
-    }
+//    function submitProgress(uint256 characterID, uint256 amount) external questsEnabled {
+//        uint256[] memory questData = getCharacterQuestData(characterID);
+//        assertOnQuest(questData);
+//    }
 
-    function submitProgressForced(uint256 characterID, uint256 amount) external restricted questsEnabled {
-
-    }
+//    function submitProgressForced(uint256 characterID, uint256 amount) external restricted questsEnabled {
+//
+//    }
 
 }
