2f2474d5f2dd6b4dea979805a404e397623877d3
kevinunger <kevinunger98@gmail.com>
2022-01-19 12:44:33 +0100

    revert change

--- frontend/src/rename-censor.ts
+++ frontend/src/rename-censor.ts
@@ -2,9 +2,6 @@ import { CensorSensor } from 'censor-sensor';
 
 const censor = new CensorSensor();
 
-//disable censor for tier 2,3,4
-for (let i = 4; i > 1; i--) censor.disableTier(i);
-
 export const getCleanName = (name: string): string => {
   return censor.cleanProfanityIsh(name);
 };
