1ec2cd1e24ab15cb581bf9b328cc63039e9495cf
Kacper <kacpertokarczyk1@gmail.com>
2022-01-20 19:18:35 +0100

    limit migration to testnets only

--- migrations/121_character_burn.js
+++ migrations/121_character_burn.js
@@ -6,19 +6,29 @@ const CryptoBlades = artifacts.require("CryptoBlades");
 const BurningManager = artifacts.require("BurningManager");
 
 module.exports = async function (deployer, network) {
-  let nftMarket = await upgradeProxy(NFTMarket.address, NFTMarket, { deployer });
-  let characters = await upgradeProxy(Characters.address, Characters, { deployer });
-  let garrison = await upgradeProxy(Garrison.address, Garrison, { deployer });
-  let game = await CryptoBlades.deployed();
-  let burningManager = await deployProxy(BurningManager, [characters.address, garrison.address, game.address, '120000000000000000'], { deployer });
+  if (network === "development"
+    || network === "development-fork"
+    || network === 'bsctestnet'
+    || network === 'bsctestnet-fork'
+    || network === 'hecotestnet'
+    || network === 'okextestnet'
+    || network === 'polygontestnet'
+    || network === 'avaxtestnet'
+    || network === 'avaxtestnet-fork') {
+    let nftMarket = await upgradeProxy(NFTMarket.address, NFTMarket, { deployer });
+    let characters = await upgradeProxy(Characters.address, Characters, { deployer });
+    let garrison = await upgradeProxy(Garrison.address, Garrison, { deployer });
+    let game = await CryptoBlades.deployed();
+    let burningManager = await deployProxy(BurningManager, [characters.address, garrison.address, game.address, '120000000000000000'], { deployer });
 
-  let BURNER_ROLE = await burningManager.BURNER_ROLE();
-  await burningManager.grantRole(BURNER_ROLE, nftMarket.address);
-  await nftMarket.migrateTo_29635ef(burningManager.address);
+    let BURNER_ROLE = await burningManager.BURNER_ROLE();
+    await burningManager.grantRole(BURNER_ROLE, nftMarket.address);
+    await nftMarket.migrateTo_29635ef(burningManager.address);
 
-  let GAME_ADMIN = await game.GAME_ADMIN();
-  await game.grantRole(GAME_ADMIN, burningManager.address);
+    let GAME_ADMIN = await game.GAME_ADMIN();
+    await game.grantRole(GAME_ADMIN, burningManager.address);
 
-  let characters_GAME_ADMIN = await characters.GAME_ADMIN();
-  await characters.grantRole(characters_GAME_ADMIN, burningManager.address);
+    let characters_GAME_ADMIN = await characters.GAME_ADMIN();
+    await characters.grantRole(characters_GAME_ADMIN, burningManager.address);
+  }
 };
