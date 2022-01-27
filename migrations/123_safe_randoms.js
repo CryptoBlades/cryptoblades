2dc9026828a646016efd1ae010e924b0c464e196
PoshDan <karsai.daniel.93@gmail.com>
2022-01-27 18:04:00 +0100

    deleted old seed vault file, included weapons contract upgrade for mintN

--- migrations/123_safe_randoms.js
+++ migrations/123_safe_randoms.js
@@ -1,15 +1,12 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 const SafeRandoms = artifacts.require("SafeRandoms");
-const CryptoBlades = artifacts.require("CryptoBlades");
+const Weapons = artifacts.require("Weapons");
 
 module.exports = async function (deployer, network, accounts) {
   const safeRandoms = await deployProxy(SafeRandoms, [], { deployer });
   const GAME_ADMIN = await safeRandoms.GAME_ADMIN();
   await safeRandoms.grantRole(GAME_ADMIN, accounts[0]);
 
-  // test stuff
-  /*const game = await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
-  await game.setSafeRandom(safeRandoms.address);
-  await safeRandoms.grantRole(GAME_ADMIN, game.address);*/
+  const weps = await upgradeProxy(Weapons.address, Weapons, { deployer });
 };
