d5ebee10687eef82c41feec06fc19cd58697a75f
PoshDan <karsai.daniel.93@gmail.com>
2022-01-27 03:26:49 +0100

    implemented safe randoms (todo slight cleanup)

--- migrations/123_safe_randoms.js
+++ migrations/123_safe_randoms.js
@@ -1,9 +1,15 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 const SafeRandoms = artifacts.require("SafeRandoms");
+const CryptoBlades = artifacts.require("CryptoBlades");
 
 module.exports = async function (deployer, network, accounts) {
   const safeRandoms = await deployProxy(SafeRandoms, [], { deployer });
-  const SEED_AUTHORITY = await safeRandoms.SEED_AUTHORITY();
-  await safeRandoms.grantRole(SEED_AUTHORITY, accounts[0]);
+  const GAME_ADMIN = await safeRandoms.GAME_ADMIN();
+  await safeRandoms.grantRole(GAME_ADMIN, accounts[0]);
+
+  // test stuff
+  /*const game = await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
+  await game.setSafeRandom(safeRandoms.address);
+  await safeRandoms.grantRole(GAME_ADMIN, game.address);*/
 };
