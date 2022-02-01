de040f8e6e6bcbb552b2048e45352022873b2ef8
Kacper <kacpertokarczyk1@gmail.com>
2022-02-01 12:42:38 +0100

    add giveawaySoul function for giveaways

--- migrations/128_bonus_power_bridge.js
+++ migrations/128_bonus_power_bridge.js
@@ -2,8 +2,10 @@ const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 const Characters = artifacts.require("Characters");
 const NFTStorage = artifacts.require("NFTStorage");
+const BurningManager = artifacts.require("BurningManager");
 
 module.exports = async function (deployer, network) {
     await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
     await upgradeProxy(Characters.address, Characters, { deployer });
+    await upgradeProxy(BurningManager.address, BurningManager, { deployer });
 };
