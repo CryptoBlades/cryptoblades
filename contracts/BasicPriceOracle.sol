d36ae29494fe206b653d3e46ecfcb30096c65e68
Brian Manthos <manthos@msn.com>
2022-02-03 16:28:35 -0800

    add registerListener() to IPriceOracle

--- contracts/BasicPriceOracle.sol
+++ contracts/BasicPriceOracle.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.6.5;
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "./interfaces/IPriceOracle.sol";
+import "./interfaces/IPriceListener.sol";
 
 contract BasicPriceOracle is IPriceOracle, Initializable, AccessControlUpgradeable {
     bytes32 public constant PRICE_UPDATER = keccak256("PRICE_UPDATER");
+    bytes32 public constant REGISTER_LISTENER = keccak256("REGISTER_LISTENER");
 
     bool private priceSet;
     uint256 private currentPrice_;
+    IPriceListener[] private listeners;
 
     function initialize() public initializer {
         __AccessControl_init();
@@ -20,6 +23,9 @@ contract BasicPriceOracle is IPriceOracle, Initializable, AccessControlUpgradeab
         currentPrice_ = 0;
     }
 
+    // TODO: Add migration
+    //      _setupRole(REGISTER_LISTENER, ...);
+
     // Views
     function currentPrice() external override view returns (uint256) {
         require(priceSet, "Price must be set beforehand");
@@ -27,6 +33,14 @@ contract BasicPriceOracle is IPriceOracle, Initializable, AccessControlUpgradeab
     }
 
     // Mutative
+    function registerListener(IPriceListener listener) external override {
+        require(hasRole(REGISTER_LISTENER, _msgSender()), "Missing REGISTER_LISTENER role");
+        require(address(listener) != address(0));
+        listeners.push(listener);
+        // This serves both set initial price and to validate the listener.
+        listener.speakCurrentPrice(currentPrice_);
+    }
+
     function setCurrentPrice(uint256 _currentPrice) external override {
         require(hasRole(PRICE_UPDATER, _msgSender()), "Missing PRICE_UPDATER role");
         require(_currentPrice > 0, "Price must be non-zero");
@@ -35,6 +49,10 @@ contract BasicPriceOracle is IPriceOracle, Initializable, AccessControlUpgradeab
         currentPrice_ = _currentPrice;
 
         emit CurrentPriceUpdated(_currentPrice);
+
+        for(uint i = 0; i < listeners.length; i++) {
+            listeners[i].speakCurrentPrice(currentPrice_);
+        }
     }
 
     // Events
--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.6.5;
 
+import "./IPriceListener.sol";
+
 interface IPriceOracle {
     // Views
     function currentPrice() external view returns (uint256 price);
 
     // Mutative
+    function registerListener(IPriceListener listener) external;
     function setCurrentPrice(uint256 price) external;
 
     // Events
