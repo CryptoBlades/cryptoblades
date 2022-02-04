d024ffe84658ff3689f7d7ec10185acf1ae28506
Cosy0o0 <diessel0o0@gmail.com>
2022-02-04 15:03:28 +0100

    added setting first project per default, added no projects text

--- frontend/src/components/smart/ClaimRewardsBar.vue
+++ frontend/src/components/smart/ClaimRewardsBar.vue
@@ -131,9 +131,8 @@
               })
             }}</h6>
         </div>
-        <div class="mt-3" v-if="!selectedPartneredProject">
-          <h5>{{withdrawalInfoText}}</h5>
-          <h6>{{$t('ClaimRewardsBar.earlyWithdrawTax')}}: {{ formattedRewardsClaimTax }} {{$t('ClaimRewardsBar.taxReduce')}} {{getTaxTimerNextTick}}</h6>
+        <div class="mt-3" v-if="isNoProjectAvailable">
+          <h5>{{$t('ClaimRewardsBar.noProjects')}}</h5>
         </div>
       </div>
     </b-modal>
@@ -148,7 +147,6 @@ import BigNumber from 'bignumber.js';
 import {RequiredXp} from '../../interfaces';
 import {ICharacter} from '@/interfaces';
 import {fromWeiEther, toBN} from '../../utils/common';
-import {secondsToDDHHMMSS} from '../../utils/date-time';
 import {getCleanName} from '../../rename-censor';
 import {SupportedProject} from '@/views/Treasury.vue';
 import PartneredProject from '../PartneredProject.vue';
@@ -262,34 +260,8 @@ export default Vue.extend({
       return (this.formattedMultiplier*100).toFixed(2) + '%';
     },
 
-    getTaxTimerNextTick(): string {
-      let frac: BigNumber;
-
-      // if has no skill rewards do not display timer next tick.
-      // or if tax is zero also do not display timer next tick.
-      if (this.skillRewards === '0' || this.rewardsClaimTaxAsFactorBN.toString() === '0') {
-        return '';
-      } else {
-        frac = this.rewardsClaimTaxAsFactorBN;
-      }
-
-      // get 2 decimal values
-      const decVal = toBN(frac.multipliedBy(100).decimalPlaces(2).toString().split('.')[1]);
-      // convert to seconds
-      const toSec = decVal.dividedBy(100).multipliedBy(24).multipliedBy(60).multipliedBy(60);
-      // return message
-      return ` Next -1% reduction happens in ${secondsToDDHHMMSS(toSec.toNumber())}.`;
-    },
-
     skillRewardNumber(): number {
       return +toBN(fromWeiEther(this.skillRewards.substr(0, this.skillRewards.length - 3) + '000'));
-    },
-
-    withdrawalInfoText(): string {
-      if(this.skillRewardNumber >= 1) {
-        return `${i18n.t('ClaimRewardsBar.withdrawalInfoText1')} ${this.formattedRemainingClaimableSkill}`;
-      }
-      return `${i18n.t('ClaimRewardsBar.withdrawalInfoText2')} ${this.formattedRemainingClaimableSkill}`;
     },
 
     xpRewardsForOwnedCharacters(): string[] {
@@ -352,6 +324,11 @@ export default Vue.extend({
 
     selectedPartneredProject(): SupportedProject | undefined {
       return this.supportedProjects.find(x => x.id === this.payoutCurrencyId);
+    },
+
+    isNoProjectAvailable(): boolean {
+      this.choosePayoutCurrencyIfNotChosenBefore();
+      return this.payoutCurrencyId === '-1';
     },
 
     canClaimSelectedProject(): boolean {
@@ -421,6 +398,13 @@ export default Vue.extend({
 
     async getRemainingTokenClaimAmountPreTax() {
       this.remainingTokenClaimAmountPreTax = await this.fetchRemainingTokenClaimAmountPreTax();
+    },
+
+    choosePayoutCurrencyIfNotChosenBefore() {
+      const supportedProjects = this.supportedProjects;
+      if(this.payoutCurrencyId === '-1' && supportedProjects.length !== 0) {
+        this.updatePayoutCurrencyId(supportedProjects[0].id);
+      }
     },
 
     getCleanCharacterName(id: number): string {
