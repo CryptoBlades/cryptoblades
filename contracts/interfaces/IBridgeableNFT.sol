7126c9081afb2f3bc66fed1246446882dc83e8dd
Moussa Khalil <moussakhalil1990@gmail.com>
2022-02-06 19:58:15 +0200

    1142-generic-nft-bridge

--- contracts/interfaces/IBridgeableNFT.sol
+++ contracts/interfaces/IBridgeableNFT.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.6.5;
 interface IBridgeableNFT {
     function collectData(uint256 tokenId) external view returns (uint256[] memory uintVars, bool[] memory boolVars, address[] memory addressVars,  string[] memory stringVars);
 
-    function mintOrUpdate(uint256 tokenId, uint256[] calldata uintVars, bool[] calldata boolVars, address[] calldata addressVars,  string[] calldata stringVars) external;
+    function mintOrUpdate(uint256 tokenId, uint256[] calldata uintVars, bool[] calldata boolVars, address[] calldata addressVars,  string[] calldata stringVars) external returns (uint256);
 }
