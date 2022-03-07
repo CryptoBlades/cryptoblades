b9255c7725e17406f5a7b9d49b9a923d01f83a49
Kacper <kacpertokarczyk1@gmail.com>
2022-03-07 20:23:27 +0100

    deployed up to migration 139 to all testnets

--- migrations/139_simple_quests_upgrade.js
+++
@@ -1,7 +0,0 @@
-const {upgradeProxy} = require("@openzeppelin/truffle-upgrades");
-
-const SimpleQuests = artifacts.require("SimpleQuests");
-
-module.exports = async function (deployer) {
-  await upgradeProxy(SimpleQuests.address, SimpleQuests, {deployer});
-};
