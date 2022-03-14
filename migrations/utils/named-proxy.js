5c950b097b3b7c638147fa61f35b17a0264b5062
Brian Manthos <manthos@msn.com>
2022-03-13 20:59:42 -0700

    Add setNamedProxyAddress()

--- migrations/utils/named-proxy.js
+++ migrations/utils/named-proxy.js
@@ -3,16 +3,19 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 async function deployNamedProxy(Contract, args, opts, name) {
   const chainId = opts.deployer.network_id;
   const contract = await deployProxy(Contract, args, opts);
-
-  if (contract.constructor._json.networks[chainId].namedProxies === undefined) {
-      contract.constructor._json.networks[chainId].namedProxies = {}
-  }
-  contract.constructor._json.networks[chainId].namedProxies[name] = contract.address;
-
+  setNamedProxyAddress(contract, name, contract.address);
   return contract;
 }
 
+function setNamedProxyAddress(contract, proxyName, proxyAddress) {
+  const chainId = contract.constructor.network_id;
+  if (contract.constructor._json.networks[chainId].namedProxies === undefined)
+      contract.constructor._json.networks[chainId].namedProxies = {}
+  contract.constructor._json.networks[chainId].namedProxies[proxyName] = proxyAddress;
+}
+
 module.exports = {
-  deployNamedProxy: deployNamedProxy
+  deployNamedProxy: deployNamedProxy,
+  setNamedProxyAddress: setNamedProxyAddress
 }
 
