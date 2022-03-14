2e6a1cb0b091c5047ce82e97668ac00864e4cf3c
Kacper <kacpertokarczyk1@gmail.com>
2022-03-14 13:50:59 +0100

    heco giveaway contract + front-end assets

---
+++ migrations/143_partner_giveaway.js
@@ -0,0 +1,11 @@
+const { upgradeProxy, deployProxy } = require('@openzeppelin/truffle-upgrades');
+
+const PartnerGiveaways = artifacts.require('PartnerGiveaways');
+const SpecialWeaponsManager = artifacts.require('SpecialWeaponsManager');
+
+module.exports = async function (deployer, network) {
+  const specialWeaponsManager = await SpecialWeaponsManager.deployed();
+  const GAME_ADMIN = await specialWeaponsManager.GAME_ADMIN();
+  const partnerGiveaways = await deployProxy(PartnerGiveaways, [specialWeaponsManager.address], { deployer });
+  await specialWeaponsManager.grantRole(GAME_ADMIN, partnerGiveaways.address);
+};
