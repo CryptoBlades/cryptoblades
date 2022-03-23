7da803cc1a35d47bee78ab01afe4e1383b29a5f7
Alvin Borromeo <88535067+Spintacle123@users.noreply.github.com>
2022-03-23 22:55:42 +0800

    1188 new game UI combat figthing (#1262)
    * Rebase, squash and remove excess code
    
    * package

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="app">
-    <nav-bar />
-    <character-bar v-if="!featureFlagStakeOnly && currentCharacterId !== null" />
+    <nav-bar :isToggled="toggleSideBar"/>
     <div class="content dark-bg-text">
-      <router-view v-if="canShowApp" />
+      <b-row>
+        <character-bar :isToggled="toggleSideBar" v-if="!featureFlagStakeOnly && currentCharacterId !== null"/>
+        <b-col :class="toggleSideBar ? 'can-show-app' : 'col-xl-9 col-lg-8 col-md-8 col-sm-10 cols-11 set-normal'">
+        <!-- :class="!featureFlagStakeOnly && currentCharacterId !== null ? 'bg-image' : ''"> -->
+          <router-view v-if="canShowApp" />
+        </b-col>
+        <WeaponRowGrid v-if="showWeapon" v-model="currentWeaponId" :checkForDurability="true"/>
+      </b-row>
     </div>
     <div class="content dark-bg-text" v-if="!canShowApp">
       {{$t('app.cantView')}}
@@ -89,6 +95,7 @@ import BigButton from './components/BigButton.vue';
 import SmallButton from './components/SmallButton.vue';
 import NavBar from './components/NavBar.vue';
 import CharacterBar from './components/CharacterBar.vue';
+import WeaponRowGrid from './components/smart/WeaponRowGrid.vue';
 import { apiUrl } from './utils/common';
 import i18n from './i18n';
 import { getConfigValue } from './contracts';
@@ -107,6 +114,7 @@ export default {
     CharacterBar,
     BigButton,
     SmallButton,
+    WeaponRowGrid
   },
 
   data: () => ({
@@ -116,6 +124,9 @@ export default {
     isConnecting: false,
     recruitCost: '',
     isOptions: false,
+    showWeapon: false,
+    currentWeaponId: null,
+    toggleSideBar: true
   }),
 
   computed: {
@@ -146,6 +157,9 @@ export default {
 
     async currentCharacterId() {
       await this.updateCharacterStamina(this.currentCharacterId);
+    },
+    currentWeaponId(){
+      Events.$emit('setWeaponId', this.currentWeaponId);
     },
     $route(to) {
       // react to route changes
@@ -323,6 +337,12 @@ export default {
     //     },
     //   );
     // });
+    Events.$on('weapon-inventory', (bol) =>{
+      this.showWeapon = bol;
+    });
+    Events.$on('toggle-sideBar', (bol) =>{
+      this.toggleSideBar = bol;
+    });
 
     document.body.addEventListener('click', (e) => {
       const tagname = e.target.getAttribute('tagname');
@@ -421,9 +441,33 @@ export default {
 
 <style>
 
+@font-face {
+    font-family: 'Trajan';
+    src: url('./assets/fonts/Trajan.ttf');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+
+
 button.btn.button.main-font.dark-bg-text.encounter-button.btn-styled.btn-primary > h1 {
   font-weight: 600;
   text-align: center;
+}
+
+.app{
+  width: auto;
+}
+
+
+.set-normal{
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  transition: 1s all;
+  background-image: url('./assets/combat-bg.png');
 }
 
 hr.hr-divider {
@@ -432,7 +476,7 @@ hr.hr-divider {
 }
 body {
   margin: 0;
-  background: linear-gradient(45deg, rgba(20, 20, 20, 1) 100%, rgba(36, 39, 32, 1) 100%);
+  background: linear-gradient(45deg, rgba(20, 20, 20, 1) 100%, #242720 100%);
 }
 
 .no-margin {
@@ -462,7 +506,12 @@ body {
 }
 
 .body {
-  max-height: calc(100vh - 56px - 160px);
+  padding-top: 15px 35px;
+  /* max-height: calc(100vh - 56px - 160px); */
+}
+
+.body  > div{
+  padding-left: 20px;
 }
 
 button,
@@ -512,10 +561,9 @@ button,
   color: grey;
 }
 
-.fire-icon,
-.str-icon {
+.fire-icon,.str-icon {
   color: red;
-  content: url('assets/elements/fire.png');
+  content: url('assets/elements/icon-fire.png');
   width: 1em;
   height: 1em;
 }
@@ -523,7 +571,7 @@ button,
 .earth-icon,
 .dex-icon {
   color: green;
-  content: url('assets/elements/earth.png');
+  content: url('assets/elements/icon-earth.png');
   width: 1em;
   height: 1em;
 }
@@ -531,7 +579,7 @@ button,
 .water-icon,
 .int-icon {
   color: cyan;
-  content: url('assets/elements/water.png');
+  content: url('assets/elements/icon-water.png');
   width: 1em;
   height: 1em;
 }
@@ -539,7 +587,7 @@ button,
 .lightning-icon,
 .cha-icon {
   color: yellow;
-  content: url('assets/elements/lightning.png');
+  content: url('assets/elements/icon-thunder.png');
   width: 1em;
   height: 1em;
 }
@@ -760,9 +808,31 @@ div.bg-success {
   border: 1px solid #9e8a57;
 }
 
+.bg-image{
+  background: url('./assets/combat-bg.png');
+  background-repeat: no-repeat;
+  background-size:cover;
+  border-radius:0px;
+}
+
+
+.can-show-app{
+  width: 100%;
+  padding-top: 40px;
+}
+
+
+
+@media all and (max-width: 600px) {
+  .can-show-app{
+    overflow-y: hidden ;
+  }
+}
+
 @media all and (max-width: 767.98px) {
   .content {
     padding: 10px;
+    padding-top: 0px;
   }
   .dark-bg-text {
     width: 100%;
---
+++ frontend/src/views/PlayToEarn.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="body main-font">
+    <div>
+      <div class="row">
+          <div class="col-lg-6 col-sm-12 adventure">
+            <h5>{{$t('PlayToEarn.playToEarn')}}</h5>
+          </div>
+      </div>
+      <img src="../assets/divider7.png" class="info-divider enemy-divider" />
+      <div class="row">
+          <div class="col-lg-12 play-to-earn">
+            <router-link v-if="!stakeOnly" :to="{ name: 'combat' }" exact class="adventures navs">
+                <div>
+                  <h4>{{$t('PlayToEarn.adventure')}}</h4>
+                  <p>{{$t('PlayToEarn.adventureDesc')}}</p>
+                  <div class="btn-join">{{$t('PlayToEarn.btnCombat')}}</div>
+                </div>
+            </router-link>
+
+            <!-- change this to raid route -->
+            <router-link v-if="!stakeOnly" :to="{ name: 'combat' }" exact class="raid navs">
+                <div>
+                  <h4>{{$t('PlayToEarn.raid')}}</h4>
+                  <p>{{$t('PlayToEarn.raidDesc')}}</p>
+                  <div class="btn-join">{{$t('PlayToEarn.btnJoin')}}</div>
+                </div>
+            </router-link>
+
+            <!-- change this to arena route -->
+            <router-link v-if="!stakeOnly" :to="{ name: 'combat' }" exact class="arena navs">
+                <div>
+                  <h4>{{$t('PlayToEarn.arena')}}</h4>
+                  <p>{{$t('PlayToEarn.arenaDesc')}}</p>
+                  <div class="btn-join">{{$t('PlayToEarn.btnPvp')}}</div>
+                </div>
+            </router-link>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {stakeOnly} from '@/feature-flags';
+import Events from '../events';
+// import gasp from 'gsap'; // for enemy card animaton
+
+export default {
+  data() {
+    return {
+      stakeOnly,
+      isToggled: false
+    };
+  },
+  async mounted(){
+    Events.$on('setWeaponId', (id) =>{
+      this.selectedWeaponId = id;
+    });
+  },
+  methods: {
+  },
+  components: {
+  },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap');
+
+h5{
+  font-family: Trajan;
+  font-size: 25px;
+  font-weight: 600;
+}
+
+.play-to-earn{
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
+}
+
+.play-to-earn > .navs{
+  border: 1px solid #9e8a57;
+  height: 30em;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-end;
+  /* box-shadow: inset 0px 0px 20px 10px rgba(0,0,0,0.6); */
+  padding-top: 30px;
+  cursor: pointer;
+}
+
+.main-font{
+  background-image: url('../assets/combat-bg.png');
+  background-position: 0 0;
+  background-size: cover;
+}
+
+.play-to-earn > .navs:hover{
+  margin-top: -20px;
+  transition: all 0.4s ease-in-out;
+}
+
+.play-to-earn > .navs > div{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-image: linear-gradient(to top, rgb(0, 0, 0),rgba(0, 0, 0, 0.542), rgba(0, 0, 0, 0.042));
+  padding: 30px 50px 40px 50px;
+}
+
+.play-to-earn > .navs > div > p{
+  font-family: Roboto;
+  color: aliceblue;
+  font-size: 13px;
+}
+
+.play-to-earn > .navs > div > h4{
+  font-family: Trajan;
+  font-weight: 800;
+  color:#9e8a57;
+}
+
+.play-to-earn > .navs > div > .btn-join{
+  width: 160px;
+  background-image: url('../assets/btn-join.png') ;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 20px;
+  font-size: 18px;
+  font-family: Roboto;
+  color: #fff;
+  font-weight: 800;
+  cursor: pointer;
+}
+
+
+.play-to-earn .adventures{
+  background-image: url('../assets/bg-orc.png');
+  background-position:center ;
+  background-size: cover;
+}
+
+.play-to-earn .raid{
+  background-image: url('../assets/bg-demon.png');
+  background-position:center ;
+  background-size: cover;
+}
+
+.play-to-earn .arena{
+  background-image: url('../assets/bg-arena.png');
+  background-position:center ;
+  background-size: cover;
+}
+
+
+.adventure{
+    text-align: left;
+  }
+
+
+.adventure > img{
+  display: none;
+}
+
+
+@media all and (max-width: 600px) {
+
+  .play-to-earn > .navs{
+    font-family: Roboto;
+    color: aliceblue;
+    font-size: 13px;
+    width: 100%;
+    /* height: 10em; */
+    flex-grow: 1;
+    background-position:center;
+  }
+
+  .play-to-earn{
+    height: 70vh;
+    display: flex;
+    justify-content: space-around;
+    flex-grow: 1;
+    flex-wrap: wrap;
+    flex: none;
+    gap: 30px;
+    margin-top: 0px;
+    overflow-x: auto;
+  }
+}
+
+</style>
