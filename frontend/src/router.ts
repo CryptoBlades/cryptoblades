2d43c0f4872e401095ea344f654cba2594c11d81
Kacper <kacpertokarczyk1@gmail.com>
2022-03-24 17:05:14 +0100

    Revert "1188 new game UI combat figthing (#1262)"
    This reverts commit 7da803cc1a35d47bee78ab01afe4e1383b29a5f7.

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -16,7 +16,6 @@ import Admin from './views/Admin.vue';
 import NftDisplay from './views/NftDisplay.vue';
 import Bridge from './views/Bridge.vue';
 import Treasury from './views/Treasury.vue';
-import PlayToEarn from './views/PlayToEarn.vue';
 
 import {market, merchandise, portal, pvp, quests, raid, stakeOnly} from './feature-flags';
 import Merchandise from '@/components/smart/Merchandise.vue';
@@ -47,7 +46,6 @@ export default function createRouter() {
       {path: '/bridge', name: 'bridge', component: Bridge},
       {path: '/treasury', name: 'treasury', component: Treasury},
       {path: '/admin', name: 'admin', component: Admin},
-      {path: '/play-to-earn', name: 'play-to-earn', component: PlayToEarn},
     ]
   });
 
