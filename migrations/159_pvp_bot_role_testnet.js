9d4da05301ab0140cad14f4dfe92ee030b5258b2
Kacper <kacpertokarczyk1@gmail.com>
2022-04-19 21:16:16 +0200

    deployed migration 159 to BSC, HECO, OEC, AVAX, AURORA TESTNETS

--- migrations/159_pvp_bot_role_testnet.js
+++ migrations/159_pvp_bot_role_testnet.js
@@ -1,17 +1,31 @@
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 const PvpCore = artifacts.require("PvpCore");
 const PvpRankings = artifacts.require("PvpRankings");
 
 module.exports = async function (deployer, network, accounts) {
+    if (
+      network === "development" ||
+      network === "development-fork" ||
+      network === 'bsctestnet' ||
+      network === 'bsctestnet-fork' ||
+      network === 'hecotestnet' ||
+      network === 'okextestnet' ||
+      network === 'polygontestnet' ||
+      network === 'avaxtestnet' ||
+      network === 'avaxtestnet-fork' ||
+      network === 'auroratestnet'
+  ) {    
+    const pvpCore = await upgradeProxy(PvpCore.address, PvpCore, { deployer });
+
     if (network === 'bsctestnet' || network === 'bsctestnet-fork') {
-        const pvpBotAddress = '0xC24658012D08a8A575Aa140C7EE45e83c9100F73'
+      const pvpBotAddress = '0xC24658012D08a8A575Aa140C7EE45e83c9100F73';
 
-        const pvpRankings = await PvpRankings.deployed()
+      const pvpRankings = await PvpRankings.deployed();
 
-        const pvpCore = await PvpCore.deployed()
+      await pvpCore.grantRole(await pvpCore.GAME_ADMIN(), pvpBotAddress);
+      await pvpRankings.grantRole(await pvpRankings.GAME_ADMIN(), pvpBotAddress);
 
-        await pvpCore.grantRole(await pvpCore.GAME_ADMIN(), pvpBotAddress);
-        await pvpRankings.grantRole(await pvpRankings.GAME_ADMIN(), pvpBotAddress);
-
-        await pvpCore.setPvpBotAddress(pvpBotAddress);
+      await pvpCore.setPvpBotAddress(pvpBotAddress);
     }
+  }
 };
