b6fbe154a68356021866ecd1b6f2ffc0de3648e1
PoshDan <karsai.daniel.93@gmail.com>
2022-05-18 07:29:43 +0200

    prepared migration for token manager slippage

--- migrations/166_tokens_manager_slippage.js
+++
@@ -1,8 +0,0 @@
-const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
-
-const TokensManager = artifacts.require("TokensManager");
-
-module.exports = async function (deployer, network) {
-
-    const tokensManager = await upgradeProxy(TokensManager.address, TokensManager, { deployer });
-};
