0b5df64e9d3d64eac78279383579af0a2be382d7
Jcalcaldev <59392491+Jcalcaldev@users.noreply.github.com>
2022-05-19 09:21:23 -0400

    set base power as level 1 on ffa arena (#1395)
    * set base power as level 1 on ffa arena
    
    * revert package json changes
    
    * lint code

---
+++ migrations/168_pvp_core_upgrade.js
@@ -0,0 +1,19 @@
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+const PvpCore = artifacts.require("PvpCore");
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === "development"
+  || network === "development-fork"
+  || network === 'bsctestnet'
+  || network === 'bsctestnet-fork'
+  || network === 'hecotestnet'
+  || network === 'okextestnet'
+  || network === 'polygontestnet'
+  || network === 'avaxtestnet'
+  || network === 'avaxtestnet-fork'
+  || network === 'auroratestnet'
+  || network === 'kavatestnet') {
+    await upgradeProxy(PvpCore.address, PvpCore, { deployer });
+  }
+};
