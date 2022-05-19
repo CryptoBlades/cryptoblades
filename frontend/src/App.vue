ac76444ecbbe7742f64d8ffd8ad07f270ebe9ede
Kevin Unger <kevinunger98@gmail.com>
2022-05-19 18:46:09 +0200

    +enrex widget

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -79,6 +79,7 @@
           </script2>
       </div>
     </div>
+    <enrex-widget/>
   </div>
 </template>
 
@@ -95,6 +96,7 @@ import SmallButton from './components/SmallButton.vue';
 import NavBar from './components/NavBar.vue';
 import CharacterBar from './components/CharacterBar.vue';
 import WeaponRowGrid from './components/smart/WeaponRowGrid.vue';
+import EnrexWidget from './components/EnrexWidget.vue';
 import { apiUrl } from './utils/common';
 import i18n from './i18n';
 import { getConfigValue } from './contracts';
@@ -178,6 +180,7 @@ export default Vue.extend({
     BigButton,
     SmallButton,
     WeaponRowGrid,
+    EnrexWidget,
   },
 
   data() {
