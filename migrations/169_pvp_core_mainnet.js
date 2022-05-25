c285c4a042c25b83db127f0d8166299fb5b05807
Kacper <kacpertokarczyk1@gmail.com>
2022-05-25 20:07:47 +0200

    deployed to migration 169 to ALL MAINNETS

---
+++ migrations/169_pvp_core_mainnet.js
@@ -0,0 +1,19 @@
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+const PvpCore = artifacts.require("PvpCore");
+const PvpRankings = artifacts.require("PvpRankings");
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === 'bscmainnet'
+    || network === 'bscmainnet-fork'
+    || network === 'hecomainnet'
+    || network === 'okexmainnet'
+    || network === 'polygonmainnet'
+    || network === 'avaxmainnet'
+    || network === 'avaxmainnet-fork'
+    || network === 'auroramainnet') {
+    await upgradeProxy(PvpCore.address, PvpCore, { deployer });
+    const pvpRankings = await PvpRankings.deployed();
+    await pvpRankings.setSeasonDuration(86400);
+  }
+};
