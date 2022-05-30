fb1d754cc1418edfaf888d2ef90d272fc31f290c
Chriserus <krzysborowiecki97@gmail.com>
2022-05-30 16:09:56 +0200

    Set soul multiplier through admin panel

--- frontend/src/components/smart/AdminTabs/BurningManagerAdmin.vue
+++ frontend/src/components/smart/AdminTabs/BurningManagerAdmin.vue
@@ -11,6 +11,16 @@
         {{ $t('admin.burningManager.mintSoul') }}
       </b-button>
     </div>
+    <h2 class="mt-2">{{
+        $t('admin.burningManager.setSoulMultiplierCurrent', {multiplier: currentSoulMultiplier})
+      }}</h2>
+    <div class="d-flex align-items-center gap-3">
+      <b-form-input v-model="newSoulMultiplier" :placeholder="$t('admin.weapons.multiplier')" number type="number"/>
+      <b-button @click="setNewSoulMultiplier()" :disabled="setNewSoulMultiplierButtonDisabled"
+                variant="primary" class="text-nowrap">
+        {{ $t('admin.burningManager.setSoulMultiplier') }}
+      </b-button>
+    </div>
   </div>
 </template>
 
@@ -21,6 +31,10 @@ import {isValidWeb3Address} from '../../../utils/common';
 
 interface StoreMappedActions {
   giveAwaySoul(payload: { user: string, soulAmount: number }): Promise<void>;
+
+  getSoulMultiplier(): Promise<number>;
+
+  setSoulMultiplier(payload: { multiplier: number }): Promise<void>;
 }
 
 interface GiveawaySoulMint {
@@ -31,6 +45,8 @@ interface GiveawaySoulMint {
 interface Data {
   giveawaySoulMint: GiveawaySoulMint;
   isLoading: boolean;
+  currentSoulMultiplier?: number;
+  newSoulMultiplier?: number;
 }
 
 export default Vue.extend({
@@ -40,12 +56,25 @@ export default Vue.extend({
         user: '',
         soulAmount: undefined,
       },
+      currentSoulMultiplier: undefined,
+      newSoulMultiplier: undefined,
       isLoading: false,
     } as Data;
   },
 
+  computed: {
+    setNewSoulMultiplierButtonDisabled(): boolean {
+      return this.newSoulMultiplier === undefined
+        || this.isLoading;
+    },
+  },
+
   methods: {
-    ...mapActions(['giveAwaySoul']) as StoreMappedActions,
+    ...mapActions([
+      'giveAwaySoul',
+      'getSoulMultiplier',
+      'setSoulMultiplier',
+    ]) as StoreMappedActions,
 
     mintGiveawaySoulDisabled(): boolean {
       return !isValidWeb3Address(this.giveawaySoulMint.user)
@@ -70,12 +99,40 @@ export default Vue.extend({
       }
     },
 
+    async setNewSoulMultiplier() {
+      if (this.newSoulMultiplier === undefined) return;
+      try {
+        this.isLoading = true;
+        await this.setSoulMultiplier({
+          multiplier: this.newSoulMultiplier,
+        });
+        await this.fetchCurrentSoulMultiplier();
+        this.newSoulMultiplier = undefined;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+
     clearInputs() {
       this.giveawaySoulMint = {
         user: '',
         soulAmount: undefined,
       };
-    }
+      this.newSoulMultiplier = undefined;
+    },
+
+    async fetchCurrentSoulMultiplier() {
+      try {
+        this.isLoading = true;
+        this.currentSoulMultiplier = await this.getSoulMultiplier();
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+
+  async mounted() {
+    await this.fetchCurrentSoulMultiplier();
   },
 });
 </script>
