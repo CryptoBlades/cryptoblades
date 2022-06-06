78c137398b0bde2bf39b6baa2d9da275ab8dff00
Kacper <kacpertokarczyk1@gmail.com>
2022-06-06 23:31:42 +0200

    fix + launchpad mainnet migration (#1479)
    * fix + launchpad mainnet migration
    
    * remove the adding for projects

---
+++ migrations/177_launchpad_mainnet.js
@@ -0,0 +1,30 @@
+const { upgradeProxy, deployProxy } = require("@openzeppelin/truffle-upgrades");
+
+const Launchpad = artifacts.require("Launchpad");
+const CryptoBlades = artifacts.require("CryptoBlades");
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === 'bscmainnet'
+    || network === 'bscmainnet-fork'
+    || network === 'hecomainnet'
+    || network === 'okexmainnet'
+    || network === 'polygonmainnet'
+    || network === 'avaxmainnet'
+    || network === 'avaxmainnet-fork'
+    || network === 'auroramainnet') {
+    const game = await CryptoBlades.deployed();
+    const launchpad = await deployProxy(Launchpad, [game.address], { deployer });
+    const GAME_ADMIN = await game.GAME_ADMIN();
+    await game.grantRole(GAME_ADMIN, launchpad.address);
+
+    // VAR_TIERS_AMOUNT = 1;
+    // VAR_FUNDING_PERIOD_PHASE_1 = 2;
+    // VAR_FUNDING_PERIOD_PHASE_2 = 3;
+    // VAR_UNCLAIMED_TO_ALLOCATION_MULTIPLIER = 4;
+    // VAR_UNCLAIMED_ALLOCATION_PERCENTAGE = 5;
+    // VAR_UNCLAIMED_COMMIT_WINDOW = 6;
+    // VAR_UNCLAIMED_COMMIT_START_OFFSET = 7;
+    await launchpad.setVars([2,3,4,5,6,7],[7200,3600,1,5,86400,172800]);
+    await launchpad.setSkillPrice('2000000000000000000');
+  }
+};
