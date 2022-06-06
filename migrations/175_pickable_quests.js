44d19ff6c8479ea104a92b265222197b55b35d5a
Kacper <kacpertokarczyk1@gmail.com>
2022-06-07 00:22:51 +0200

    deployed up to migration 177 to ALL MAINNETS

--- migrations/175_pickable_quests.js
+++ migrations/175_pickable_quests.js
@@ -2,6 +2,19 @@ const {upgradeProxy} = require('@openzeppelin/truffle-upgrades');
 
 const SimpleQuests = artifacts.require('SimpleQuests');
 
-module.exports = async function (deployer) {
-  const simpleQuests = await upgradeProxy(SimpleQuests.address, SimpleQuests, {deployer});
+module.exports = async function (deployer, network) {
+  if (network === "development"
+  || network === "development-fork"
+  || network === 'bsctestnet'
+  || network === 'bsctestnet-fork'
+  || network === 'hecotestnet'
+  || network === 'okextestnet'
+  || network === 'polygontestnet'
+  || network === 'avaxtestnet'
+  || network === 'avaxtestnet-fork'
+  || network === 'auroratestnet'
+  || network === 'kavatestnet'
+  || network === 'skaletestnet') {
+    const simpleQuests = await upgradeProxy(SimpleQuests.address, SimpleQuests, {deployer});
+  }
 };
