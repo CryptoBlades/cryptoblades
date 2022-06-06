3d3cae885dc572dfd9f64a2d3cd0d196066f1ca8
Kacper <kacpertokarczyk1@gmail.com>
2022-06-06 20:44:06 +0200

    remove the adding for projects

--- migrations/177_launchpad_mainnet.js
+++ migrations/177_launchpad_mainnet.js
@@ -26,14 +26,5 @@ module.exports = async function (deployer, network, accounts) {
     // VAR_UNCLAIMED_COMMIT_START_OFFSET = 7;
     await launchpad.setVars([2,3,4,5,6,7],[7200,3600,1,5,86400,172800]);
     await launchpad.setSkillPrice('2000000000000000000');
-
-    let commitOnly = true;
-    if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
-      commitOnly = false;
-    }
-
-    await launchpad.addNewLaunch("Bolstyr", "BOL", "https://cloudflare-ipfs.com/ipfs/QmTBcU7U1hWtGSSjPnAQqF93bYH8c1itvBs8CRoYXYpCWH", "https://cloudflare-ipfs.com/ipfs/QmZDHWx1GHEkeisPhR9FktYEB8ka8QxUvkXyP1URxEC588", "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", commitOnly)
-    await launchpad.addNewLaunch("Magic Connect", "MSHARD", "https://cloudflare-ipfs.com/ipfs/QmTsUCRaLssuDxfK4ENkvCzKk1UJGJH9ouGFsd7YHi5PE1", "https://cloudflare-ipfs.com/ipfs/QmQGfym9oLB3ETTcQCTB6RnLMk7i11wXZZfHPvyf9e9umW", "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", commitOnly)
-    await launchpad.addNewLaunch("Boom Brigade", "MEDAL", "https://cloudflare-ipfs.com/ipfs/QmPTJ7ugmRaexuQ7tUq7KYF6vvwq3Y3rcjxdwZYtZ68ZeG", "https://cloudflare-ipfs.com/ipfs/QmUFMh18omfxPXmz18UBGDZo6wmiKofaH2pjvCKyc8DQJe", "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", commitOnly)
   }
 };
