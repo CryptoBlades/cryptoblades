743afdeaa16d6a4410eed3b68d1d8739d765025c
Kevin Unger <kevinunger98@gmail.com>
2022-06-10 12:39:13 +0200

    specialWeaponsManager to module

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -68,6 +68,7 @@ import raid from './raid';
 import staking from './staking';
 import land from './land';
 import treasury from './treasury';
+import specialWeaponsManager from './specialWeaponsManager';
 
 Vue.use(Vuex);
 
@@ -79,7 +80,8 @@ export default new Vuex.Store<IState>({
     raid,
     staking,
     land,
-    treasury
+    treasury,
+    specialWeaponsManager
   },
   state: {
     web3: new Web3(),
@@ -573,10 +575,6 @@ export default new Vuex.Store<IState>({
 
     updateDustBalance(state: IState, { dustBalance }) {
       state.ownedDust = dustBalance;
-    },
-
-    updateShardsSupply(state: IState, { eventId, shardsSupply }) {
-      Vue.set(state.shardsSupply, eventId, shardsSupply);
     },
 
     updateSkillRewards(state: IState, { skillRewards }: { skillRewards: string }) {
@@ -1420,17 +1418,6 @@ export default new Vuex.Store<IState>({
           commit('updateDustBalance', { dustBalance });
         })(),
       ]);
-    },
-
-    async fetchShardsSupply({ state, commit }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const eventCount = +await SpecialWeaponsManager.methods.eventCount().call(defaultCallOptions(state));
-      for(let i = 1; i <= eventCount; i++) {
-        const eventShardsSupply = await SpecialWeaponsManager.methods.getUserSpecialShardsSupply(state.defaultAccount, i).call(defaultCallOptions(state));
-        commit('updateShardsSupply', { eventId: i, shardsSupply: +eventShardsSupply });
-      }
     },
 
     async fetchSoulBalance({ state }) {
@@ -3785,268 +3772,6 @@ export default new Vuex.Store<IState>({
       if(!Weapons || !state.defaultAccount) return;
 
       await Weapons.methods.setBurnPointMultiplier(multiplier).send({ from: state.defaultAccount, gasPrice: getGasPrice() });
-    },
-
-    async getActiveSpecialWeaponsEvents({state}) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const ids = await SpecialWeaponsManager.methods.getActiveEventsIds().call(defaultCallOptions(state));
-      const events = [];
-      for(let i = 0; i < ids.length; i++) {
-        const project = await SpecialWeaponsManager.methods.eventInfo(ids[i]).call(defaultCallOptions(state));
-        events.push({id: ids[i], ...project});
-      }
-      return events;
-    },
-
-    async startNewEvent({state}, {event}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .startNewEvent(event.name, event.element, event.period, event.supply, event.art, event.details, event.website, event.note)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async setSpecialWeaponArt({state}, {eventId, art}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .setSpecialWeaponArt(eventId, art)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async setSpecialWeaponDetails({state}, {eventId, details}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .setSpecialWeaponDetails(eventId, details)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async setSpecialWeaponWebsite({state}, {eventId, website}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .setSpecialWeaponWebsite(eventId, website)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async setSpecialWeaponNote({state}, {eventId, note}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .setSpecialWeaponNote(eventId, note)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async incrementEventCount({state}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .incrementEventCount()
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async addShards({state}, {user, eventId, shardsAmount}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .addShards(user, eventId, shardsAmount)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async privatePartnerOrder({state}, {receivers, eventId, orderOption}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .privatePartnerOrder(receivers, eventId, orderOption)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async privatePartnerMint({state}, {receivers, eventId, orderOption}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .privatePartnerMint(receivers, eventId, orderOption)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async reserveForGiveaways({state}, {reservingAddress, eventId, orderOption, amount}) {
-      const {SpecialWeaponsManager} = state.contracts();
-      if (!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods
-        .reserveForGiveaways(reservingAddress, eventId, orderOption, amount)
-        .send({from: state.defaultAccount, gasPrice: getGasPrice()});
-    },
-
-    async fetchSpecialWeaponEvents({ state, dispatch, commit }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const activeSpecialWeaponEventsIds = await SpecialWeaponsManager.methods.getActiveEventsIds().call(defaultCallOptions(state));
-      commit('updateActiveSpecialWeaponEventsIds', activeSpecialWeaponEventsIds);
-
-      const eventCount = await SpecialWeaponsManager.methods.eventCount().call(defaultCallOptions(state));
-      const inactiveSpecialWeaponEventsIds = Array.from({length: +eventCount}, (_, i) =>
-        (i + 1).toString()).filter(id => !activeSpecialWeaponEventsIds.includes(id));
-      commit('updateInactiveSpecialWeaponEventsIds', inactiveSpecialWeaponEventsIds);
-
-      await dispatch('fetchSpecialWeaponEventsInfo', activeSpecialWeaponEventsIds.concat(inactiveSpecialWeaponEventsIds));
-      await dispatch('fetchShardsSupply');
-    },
-
-    async fetchSpecialWeaponArts({ state, commit }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const allSpecialWeaponEventsIds = state.activeSpecialWeaponEventsIds.concat(state.inactiveSpecialWeaponEventsIds);
-      for (let i = 0; i < allSpecialWeaponEventsIds.length; i++) {
-        const eventId = allSpecialWeaponEventsIds[i];
-        const art = await SpecialWeaponsManager.methods.specialWeaponArt(eventId).call(defaultCallOptions(state));
-        commit('updateSpecialWeaponArt', { eventId, art });
-      }
-    },
-
-    async fetchSpecialWeaponEventsInfo({ state, commit }, eventsIds) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      eventsIds.forEach(async (eventId: string) => {
-        const eventInfoRaw = await SpecialWeaponsManager.methods.getEventInfo(eventId).call(defaultCallOptions(state));
-        const ordered = +await SpecialWeaponsManager.methods.userOrderOptionForEvent(state.defaultAccount!, eventId).call(defaultCallOptions(state)) > 0;
-        const forged = await SpecialWeaponsManager.methods.userForgedAtEvent(state.defaultAccount!, eventId).call(defaultCallOptions(state));
-        const eventData = await SpecialWeaponsManager.methods.getSpecialWeaponData(eventId).call(defaultCallOptions(state));
-        const eventInfo = {
-          name: eventInfoRaw[0],
-          weaponElement: eventInfoRaw[1],
-          endTime: eventInfoRaw[2],
-          supply: eventInfoRaw[3],
-          orderedCount: eventInfoRaw[4],
-          ordered,
-          forged,
-          art: eventData[0],
-          details: eventData[1],
-          website: eventData[2],
-          notes: eventData[3]
-        };
-
-        commit('updateSpecialWeaponEventsInfo', { eventId, eventInfo });
-      });
-    },
-
-    async fetchForgeCosts({ state }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const shardsCostLow = await SpecialWeaponsManager.methods.vars(1).call(defaultCallOptions(state));
-      const shardsCostMedium = await SpecialWeaponsManager.methods.vars(2).call(defaultCallOptions(state));
-      const shardsCostHigh = await SpecialWeaponsManager.methods.vars(3).call(defaultCallOptions(state));
-      const skillCostLow = await SpecialWeaponsManager.methods.getSkillForgeCost(1).call(defaultCallOptions(state));
-      const skillCostMedium = await SpecialWeaponsManager.methods.getSkillForgeCost(2).call(defaultCallOptions(state));
-      const skillCostHigh = await SpecialWeaponsManager.methods.getSkillForgeCost(3).call(defaultCallOptions(state));
-
-      return [+shardsCostLow, +shardsCostMedium, +shardsCostHigh, +skillCostLow, +skillCostMedium, +skillCostHigh];
-    },
-
-    async convertShards({ state, dispatch }, { eventFromId, eventToId, amount }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods.convertShards(eventFromId, eventToId, amount).send({ from: state.defaultAccount, gasPrice: getGasPrice() });
-
-      await dispatch('fetchShardsSupply');
-    },
-
-    async fetchShardsConvertDenominator({ state }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      return +await SpecialWeaponsManager.methods.vars(10).call(defaultCallOptions(state));
-    },
-
-    async orderSpecialWeapon({ state, dispatch }, { eventId, orderOption, orderWithSkill }) {
-      const { SpecialWeaponsManager, CryptoBlades, SkillToken } = state.contracts();
-      if(!SpecialWeaponsManager || !CryptoBlades || !SkillToken || !state.defaultAccount) return;
-
-      if(orderWithSkill) {
-        const price = await SpecialWeaponsManager.methods.getSkillForgeCost(orderOption).call(defaultCallOptions(state));
-        await approveFeeWalletOnly(
-          CryptoBlades,
-          SkillToken,
-          state.defaultAccount,
-          defaultCallOptions(state),
-          defaultCallOptions(state),
-          new BigNumber(price)
-        );
-        await SpecialWeaponsManager.methods.orderSpecialWeaponWithSkill(eventId, orderOption).send({ from: state.defaultAccount, gasPrice: getGasPrice() });
-      }
-      else {
-        await SpecialWeaponsManager.methods.orderSpecialWeaponWithShards(eventId, orderOption).send({ from: state.defaultAccount, gasPrice: getGasPrice() });
-      }
-
-      await Promise.all([
-        dispatch('fetchForgingStatus', eventId),
-        dispatch('fetchShardsSupply'),
-        dispatch('fetchSkillBalance')
-      ]);
-    },
-
-    async forgeSpecialWeapon({ state, dispatch }, eventId) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods.forgeSpecialWeapon(eventId).send({ from: state.defaultAccount, gasPrice: getGasPrice() });
-
-      await Promise.all([
-        dispatch('updateWeaponIds'),
-        dispatch('fetchForgingStatus', eventId),
-      ]);
-    },
-
-    async fetchForgingStatus({ state, commit }, eventId) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const ordered = +await SpecialWeaponsManager.methods.userOrderOptionForEvent(state.defaultAccount!, eventId).call(defaultCallOptions(state)) > 0;
-      const forged = await SpecialWeaponsManager.methods.userForgedAtEvent(state.defaultAccount, eventId).call(defaultCallOptions(state));
-
-      commit('updateForgingStatus', { eventId, ordered, forged });
-    },
-
-    async fetchEventTotalOrderedCount({ state, commit }, eventId) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      const orderedCount = await SpecialWeaponsManager.methods.getTotalOrderedCount(eventId).call(defaultCallOptions(state));
-
-      commit('updateEventTotalOrderedCount', { eventId, orderedCount });
-    },
-
-    async fetchShardsStakingRewards({ state }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      return await SpecialWeaponsManager.methods.getUserShardsRewards(state.defaultAccount).call(defaultCallOptions(state));
-    },
-
-    async claimShardsStakingRewards({ state, dispatch }, { eventId, amount }) {
-      const { SpecialWeaponsManager } = state.contracts();
-      if(!SpecialWeaponsManager || !state.defaultAccount) return;
-
-      await SpecialWeaponsManager.methods.claimShardRewards(eventId, amount).send(defaultCallOptions(state));
-
-      await dispatch('fetchShardsSupply');
     },
 
     async fetchMintWeaponPriceDecreasePerSecond({state}) {
