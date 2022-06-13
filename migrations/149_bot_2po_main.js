4e68544efa818283d2e2f30f65a48f38485fad90
Kacper <kacpertokarczyk1@gmail.com>
2022-06-13 21:46:42 +0200

    deployed to migration 177 to KAVA mainnet

--- migrations/149_bot_2po_main.js
+++ migrations/149_bot_2po_main.js
@@ -21,7 +21,8 @@ module.exports = async function (deployer, network) {
   || network === 'avaxmainnet'
   || network === 'avaxmainnet-fork'
   || network === 'auroramainnet'
-  || network === 'skalemainnet') {
+  || network === 'skalemainnet'
+  || network === 'kavamainnet') {
     let storage = await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
     
     let weapons = await Weapons.deployed();
