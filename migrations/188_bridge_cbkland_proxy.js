3cfd33fe26c3e84ea12c31e6e7d58c6bfa9cce9c
PoshDan <karsai.daniel.93@gmail.com>
2022-09-19 16:03:18 +0200

    prepared migration 188 for mainnets (removed testnet if clause)

--- migrations/188_bridge_cbkland_proxy.js
+++ migrations/188_bridge_cbkland_proxy.js
@@ -5,31 +5,19 @@ const CBKLandBridgeProxyContract = artifacts.require("CBKLandBridgeProxyContract
 const CBKLand = artifacts.require("CBKLand");
 
 module.exports = async function (deployer, network) {
-  if (network === "development"
-  || network === "development-fork"
-  || network === 'bsctestnet'
-  || network === 'bsctestnet-fork'
-  || network === 'hecotestnet'
-  || network === 'okextestnet'
-  || network === 'polygontestnet'
-  || network === 'avaxtestnet'
-  || network === 'avaxtestnet-fork'
-  || network === 'auroratestnet'
-  || network === 'kavatestnet'
-  || network === 'skaletestnet') {
-    let storage = await NFTStorage.deployed();
-    let cbkLand = await upgradeProxy(CBKLand.address, CBKLand, { deployer });
-    let cbkLandProxy = await deployProxy(CBKLandBridgeProxyContract, [storage.address, cbkLand.address], { deployer });
-    
-    await storage.setProxyContract(cbkLand.address, cbkLandProxy.address, true);
-    
-    let cbkLandGM = await cbkLand.GAME_ADMIN();
-    let cbkLandProxyGM = await cbkLandProxy.GAME_ADMIN();
-    
-    await cbkLand.grantRole(cbkLandGM, cbkLandProxy.address);
+  
+  let storage = await NFTStorage.deployed();
+  let cbkLand = await upgradeProxy(CBKLand.address, CBKLand, { deployer });
+  let cbkLandProxy = await deployProxy(CBKLandBridgeProxyContract, [storage.address, cbkLand.address], { deployer });
+  
+  await storage.setProxyContract(cbkLand.address, cbkLandProxy.address, true);
+  
+  let cbkLandGM = await cbkLand.GAME_ADMIN();
+  let cbkLandProxyGM = await cbkLandProxy.GAME_ADMIN();
+  
+  await cbkLand.grantRole(cbkLandGM, cbkLandProxy.address);
 
-    await cbkLandProxy.grantRole(cbkLandProxyGM, storage.address);
-    
-    await storage.allowToken(cbkLand.address);
-  }
+  await cbkLandProxy.grantRole(cbkLandProxyGM, storage.address);
+  
+  await storage.allowToken(cbkLand.address);
 };
