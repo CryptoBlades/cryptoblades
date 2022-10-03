b03d3a48e557922a948724828faffa7ee1de0b19
Kacper <kacpertokarczyk1@gmail.com>
2022-10-03 15:09:19 +0200

    remove redundant code in valor staking

--- contracts/ValorStakingRewardsUpgradeable.sol
+++ contracts/ValorStakingRewardsUpgradeable.sol
@@ -1,54 +1,6 @@
 pragma solidity ^0.6.2;
 
 import "./staking/StakingRewardsUpgradeable.sol";
-import "./interfaces/IStakeFromGame.sol";
-import "./cryptoblades.sol";
 
-contract ValorStakingRewardsUpgradeable is
-    StakingRewardsUpgradeable
-{
-
-    function stake(uint256 amount)
-        external
-        override
-        normalMode
-        nonReentrant
-        whenNotPaused
-        updateReward(msg.sender)
-    {
-        _stake(msg.sender, amount);
-        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
-    }
-
-    function withdraw(uint256 amount)
-        public
-        override
-        normalMode
-        nonReentrant
-        updateReward(msg.sender)
-    {
-        require(
-            minimumStakeTime == 0 ||
-                block.timestamp.sub(_stakeTimestamp[msg.sender]) >=
-                minimumStakeTime ||
-                periodFinish <= block.timestamp,
-            "Cannot withdraw until minimum staking time has passed"
-        );
-        _unstake(msg.sender, amount);
-        stakingToken.safeTransfer(msg.sender, amount);
-    }
-
-    function exit() external override normalMode {
-        withdraw(_balances[msg.sender]);
-        getReward();
-    }
-
-    function recoverOwnStake() external override failsafeMode {
-        uint256 amount = _balances[msg.sender];
-        if (amount > 0) {
-            _totalSupply = _totalSupply.sub(amount);
-            _balances[msg.sender] = _balances[msg.sender].sub(amount);
-            stakingToken.safeTransfer(msg.sender, amount);
-        }
-    }
+contract ValorStakingRewardsUpgradeable is StakingRewardsUpgradeable {
 }
