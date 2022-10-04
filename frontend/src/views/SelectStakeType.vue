0a8cc20d7c01bcd6b7a297fe80277de3461bf983
Chriserus <krzysborowiecki97@gmail.com>
2022-10-04 22:05:37 +0200

    Add Land Stakings section

--- frontend/src/views/SelectStakeType.vue
+++ frontend/src/views/SelectStakeType.vue
@@ -21,6 +21,25 @@
           :walletBalance="staking[e.stakeType].ownBalance"/>
       </li>
     </ul>
+    <h2 class="text-center">LAND STAKINGS</h2>
+    <ul class="stake-select-list">
+      <li class="stake-select-item" v-for="e in landEntries" :key="e.stakeType">
+        <stake-selector-item
+          :isLoading="e.isLoading"
+          :stakeTitle="e.stakeTitle"
+          :stakeTokenName="e.stakeTokenName"
+          :rewardTokenName="e.rewardTokenName"
+          :stakeType="e.stakeType"
+          :minimumStakeTime="stakeOverviews[e.stakeType].minimumStakeTime"
+          :estimatedYield="estimatedYields[e.stakeType]"
+          :rewardsDuration="stakeOverviews[e.stakeType].rewardsDuration"
+          :deprecated="e.deprecated"
+          :rewardDistributionTimeLeft="stakeOverviews[e.stakeType].rewardDistributionTimeLeft"
+          :currentRewardEarned="staking[e.stakeType].currentRewardEarned"
+          :totalStaked="staking[e.stakeType].contractBalance"
+          :walletBalance="staking[e.stakeType].ownBalance"/>
+      </li>
+    </ul>
     <h2 class="text-center">NORMAL STAKINGS</h2>
     <ul class="stake-select-list">
       <li class="stake-select-item" v-for="e in nonEventEntries" :key="e.stakeType">
@@ -92,6 +111,14 @@ export default Vue.extend({
       }));
     },
 
+    landEntries() {
+      return this.availableNftStakeTypes.filter(stakeType => ['cbkLandT1', 'cbkLandT2', 'cbkLandT3'].includes(stakeType)).map(stakeType => ({
+        stakeType, ...humanReadableDetailsForNftStakeTypes[stakeType],
+        // make the isLoading = true as a default value to set the loader as initial display
+        isLoading: true
+      }));
+    },
+
     nonEventEntries() {
       return this.availableStakeTypes.filter(stakeType => !['skill60','valor','lpValor','lpValor2'].includes(stakeType)).map(stakeType => ({
         stakeType, ...humanReadableDetailsForStakeTypes[stakeType],
@@ -138,6 +165,11 @@ export default Vue.extend({
 
     setIsLoading(stakeType){
       this.eventEntries.forEach((entry) =>{
+        if(stakeType === entry.stakeType){
+          entry.isLoading = false;
+        }
+      });
+      this.landEntries.forEach((entry) =>{
         if(stakeType === entry.stakeType){
           entry.isLoading = false;
         }
