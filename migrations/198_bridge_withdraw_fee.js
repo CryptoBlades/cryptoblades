3df4666a6969ff6e64c89c7b2603dff75b096237
Kacper <kacpertokarczyk1@gmail.com>
2022-10-12 16:21:35 +0200

    deployed to TESTNETS (BNB, HECO, OKEX, AVAX)

--- migrations/198_bridge_withdraw_fee.js
+++ migrations/198_bridge_withdraw_fee.js
@@ -1,7 +1,13 @@
 const { upgradeProxy, deployProxy} = require("@openzeppelin/truffle-upgrades");
 
 const NFTStorage = artifacts.require("NFTStorage");
+const Characters = artifacts.require("Characters");
+const Weapons = artifacts.require("Weapons");
+const Shields = artifacts.require("Shields");
 
 module.exports = async function (deployer) {
-  await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
+  const nftStorage = await upgradeProxy(NFTStorage.address, NFTStorage, { deployer });
+  await nftStorage.setWithdrawFromStorageNativeFee(Characters.address, '3000000000000000');
+  await nftStorage.setWithdrawFromStorageNativeFee(Weapons.address, '3000000000000000');
+  await nftStorage.setWithdrawFromStorageNativeFee(Shields.address, '3000000000000000');
 };
