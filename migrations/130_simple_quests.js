4c8d7381824c8faca09cbfb625e41400b621dbeb
Chriserus <krzysborowiecki97@gmail.com>
2022-10-19 09:24:24 +0200

    Use Promise.all in quests migrations

--- migrations/130_simple_quests.js
+++ migrations/130_simple_quests.js
@@ -30,15 +30,12 @@ module.exports = async function (deployer, network, accounts) {
     const junk = await upgradeProxy(Junk.address, Junk, {deployer});
     const trinket = await upgradeProxy(RaidTrinket.address, RaidTrinket, {deployer});
     const shields = await upgradeProxy(Shields.address, Shields, {deployer});
+    await upgradeProxy(Raid1.address, Raid1, {deployer});
+    await upgradeProxy(NFTStorage.address, NFTStorage, {deployer});
     const burningManager = await upgradeProxy(BurningManager.address, BurningManager, {deployer});
     const safeRandoms = await SafeRandoms.deployed();
     const partnerVault = await deployProxy(PartnerVault, [], {deployer});
-    await upgradeProxy(Raid1.address, Raid1, {deployer});
-    await upgradeProxy(NFTStorage.address, NFTStorage, {deployer});
     const simpleQuests = await deployProxy(SimpleQuests, [characters.address, weapons.address, junk.address, trinket.address, shields.address, burningManager.address, safeRandoms.address, partnerVault.address], {deployer});
-    // TODO: What should be the initial values here?
-    // const VAR_COMMON_TIER = 0;
-    // await simpleQuests.setVar(VAR_COMMON_TIER, 0); Leaving this as a comment, because it's 0 by default
     const VAR_UNCOMMON_TIER = await simpleQuests.VAR_UNCOMMON_TIER();
     const VAR_RARE_TIER = await simpleQuests.VAR_RARE_TIER();
     const VAR_EPIC_TIER = await simpleQuests.VAR_EPIC_TIER();
@@ -48,27 +45,52 @@ module.exports = async function (deployer, network, accounts) {
     const VAR_REPUTATION_LEVEL_4 = await simpleQuests.VAR_REPUTATION_LEVEL_4();
     const VAR_REPUTATION_LEVEL_5 = await simpleQuests.VAR_REPUTATION_LEVEL_5();
     const VAR_SKIP_QUEST_STAMINA_COST = await simpleQuests.VAR_SKIP_QUEST_STAMINA_COST();
-    await simpleQuests.setVar(VAR_UNCOMMON_TIER, 1);
-    await simpleQuests.setVar(VAR_RARE_TIER, 2);
-    await simpleQuests.setVar(VAR_EPIC_TIER, 3);
-    await simpleQuests.setVar(VAR_LEGENDARY_TIER, 4);
-    await simpleQuests.setVar(VAR_REPUTATION_LEVEL_2, 1000);
-    await simpleQuests.setVar(VAR_REPUTATION_LEVEL_3, 2000);
-    await simpleQuests.setVar(VAR_REPUTATION_LEVEL_4, 5000);
-    await simpleQuests.setVar(VAR_REPUTATION_LEVEL_5, 10000);
-    await simpleQuests.setVar(VAR_SKIP_QUEST_STAMINA_COST, 40);
-    await simpleQuests.setTierChances(0, [100, 100, 100, 100]);
-    await simpleQuests.setTierChances(1, [85, 100, 100, 100]);
-    await simpleQuests.setTierChances(2, [77, 97, 100, 100]);
-    await simpleQuests.setTierChances(3, [69, 94, 100, 100]);
-    await simpleQuests.setTierChances(4, [66, 93, 99, 100]);
+    const VAR_CONTRACT_ENABLED = await simpleQuests.VAR_CONTRACT_ENABLED();
 
-    await characters.grantRole(await characters.GAME_ADMIN(), simpleQuests.address);
-    await weapons.grantRole(await weapons.GAME_ADMIN(), simpleQuests.address);
-    await junk.grantRole(await junk.GAME_ADMIN(), simpleQuests.address);
-    await trinket.grantRole(await trinket.GAME_ADMIN(), simpleQuests.address);
-    await shields.grantRole(await shields.GAME_ADMIN(), simpleQuests.address);
-    await burningManager.grantRole(await burningManager.GAME_ADMIN(), simpleQuests.address);
-    await safeRandoms.grantRole(await safeRandoms.GAME_ADMIN(), simpleQuests.address);
+    await simpleQuests.setVars([
+      // VAR_COMMON_TIER,
+      VAR_UNCOMMON_TIER,
+      VAR_RARE_TIER,
+      VAR_EPIC_TIER,
+      VAR_LEGENDARY_TIER,
+      VAR_REPUTATION_LEVEL_2,
+      VAR_REPUTATION_LEVEL_3,
+      VAR_REPUTATION_LEVEL_4,
+      VAR_REPUTATION_LEVEL_5,
+      VAR_SKIP_QUEST_STAMINA_COST,
+      // VAR_WEEKLY_COMPLETIONS_GOAL,
+      VAR_CONTRACT_ENABLED
+    ], [
+      // 0,
+      1,
+      2,
+      3,
+      4,
+      25,
+      120,
+      375,
+      1000,
+      40,
+      // 5,
+      1
+    ]);
+
+    await Promise.all([
+      simpleQuests.setTierChances(0, [100, 100, 100, 100]),
+      simpleQuests.setTierChances(1, [85, 100, 100, 100]),
+      simpleQuests.setTierChances(2, [77, 97, 100, 100]),
+      simpleQuests.setTierChances(3, [69, 94, 100, 100]),
+      simpleQuests.setTierChances(4, [66, 93, 99, 100]),
+    ]);
+
+    await Promise.all([
+      characters.grantRole(await characters.GAME_ADMIN(), simpleQuests.address),
+      weapons.grantRole(await weapons.GAME_ADMIN(), simpleQuests.address),
+      junk.grantRole(await junk.GAME_ADMIN(), simpleQuests.address),
+      trinket.grantRole(await trinket.GAME_ADMIN(), simpleQuests.address),
+      shields.grantRole(await shields.GAME_ADMIN(), simpleQuests.address),
+      burningManager.grantRole(await burningManager.GAME_ADMIN(), simpleQuests.address),
+      safeRandoms.grantRole(await safeRandoms.GAME_ADMIN(), simpleQuests.address),
+    ]);
   }
 };
