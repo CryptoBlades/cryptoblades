5c362e16e0cf043a5f5be3d1012de74c247f8df2
Kacper <kacpertokarczyk1@gmail.com>
2022-11-11 20:49:31 +0100

    deployed migration 206 to ALL MAINNETS, added recovery option to skill staking contract

--- migrations/206_hold_nft_per_quest_and_staking.js
+++ migrations/206_hold_nft_per_quest_and_staking.js
@@ -2,8 +2,10 @@ const {upgradeProxy} = require('@openzeppelin/truffle-upgrades');
 
 const PartnerVault = artifacts.require("PartnerVault");
 const SimpleQuests = artifacts.require("SimpleQuests");
+const SkillStakingRewardsUpgradeable = artifacts.require("SkillStakingRewardsUpgradeable");
 
 module.exports = async function (deployer) {
   await upgradeProxy(PartnerVault.address, PartnerVault, {deployer});
   await upgradeProxy(SimpleQuests.address, SimpleQuests, {deployer});
+  await upgradeProxy(SkillStakingRewardsUpgradeable.address, SkillStakingRewardsUpgradeable, {deployer});
 };
