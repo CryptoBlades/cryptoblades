abfec3d4f026d66065cf8af2e547bf36573f9801
PoshDan <karsai.daniel.93@gmail.com>
2022-11-23 16:42:59 +0100

    automatic setting of cb var from tokensmanager percentage setter

--- migrations/209_untaxed_fights_op.js
+++ migrations/209_untaxed_fights_op.js
@@ -1,9 +1,11 @@
 const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 const CryptoBlades = artifacts.require("CryptoBlades");
+const TokensManager = artifacts.require("TokensManager");
 
 module.exports = async function (deployer, network) {
     const game = await upgradeProxy(CryptoBlades.address, CryptoBlades, { deployer });
+    await upgradeProxy(TokensManager.address, TokensManager, { deployer });
     if(network === 'bscmainnet'
     || network === 'bsctestnet'
     || network === 'bsctestnet-fork') {
