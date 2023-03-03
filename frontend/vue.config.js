bcd0eca76e9917782ecddfd3384decd3d2db9a76
jeffyzxc <jeffersonnilo24@gmail.com>
2023-03-04 05:24:45 +0800

    - memory limit to 2100

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('fork-ts-checker').tap(args => {
-      args[0].memoryLimit = 2800;
+      args[0].memoryLimit = 2100;
       return args;
     });
   }
