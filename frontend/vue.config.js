fad3e4b4cdf142596dda1e216a973979e7ebe68d
jeffyzxc <jeffersonnilo24@gmail.com>
2023-03-04 05:02:16 +0800

    set to 2048

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('fork-ts-checker').tap(args => {
-      args[0].memoryLimit = 1024;
+      args[0].memoryLimit = 2048;
       return args;
     });
   }
