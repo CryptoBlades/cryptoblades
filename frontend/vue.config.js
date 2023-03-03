eeef226c06a4aa50e95fd1f86f9394995996ce07
jeffyzxc <jeffersonnilo24@gmail.com>
2023-03-04 05:40:24 +0800

    - last test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  lintOnSave: false,
   chainWebpack: config => {
+    config.module.rules.delete('eslint');
+
     config.plugin('fork-ts-checker').tap(args => {
-      args[0].memoryLimit = 2100;
+      args[0].memoryLimit = 2000;
       return args;
     });
   }
