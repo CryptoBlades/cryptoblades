df65a387b815097711eb98c82b68b0635e5b446b
jeffyzxc <jeffersonnilo24@gmail.com>
2023-05-31 16:33:04 +0800

    - team fight script

---
+++ migrations/215_team_fights.js
@@ -0,0 +1,7 @@
+const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+const TokensManager= artifacts.require("TokensManager");
+
+module.exports = async function (deployer) {
+  await upgradeProxy(TokensManager.address, TokensManager, {deployer});
+};
